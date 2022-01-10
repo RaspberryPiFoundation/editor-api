53896b30e274d67182ed55dc4128e6814a608635
Lois Wells <lois.wells@raspberrypi.org>
2022-01-10 17:04:50 +0000

    Added step before validation to check identifier is unique and not null

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,14 @@
 class Project < ApplicationRecord
+  before_validation :check_unique_not_null, on: :create
   has_many :components, dependent: :destroy
+
+  require 'phrase_identifier'
+
+  private
+    def check_unique_not_null
+      self.identifier ||= PhraseIdentifier.generate
+      while !Project.find_by(identifier: self.identifier).nil?
+        self.identifier = PhraseIdentifier.generate
+      end
+    end
 end
