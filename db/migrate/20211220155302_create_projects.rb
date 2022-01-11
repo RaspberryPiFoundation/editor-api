542ab975e6944bdcd405a87ca0c65f1476df3c9d
Lois Wells <lois.wells@raspberrypi.org>
2022-01-11 14:17:18 +0000

    Moving migration change into separate migration

--- db/migrate/20211220155302_create_projects.rb
+++ db/migrate/20211220155302_create_projects.rb
@@ -3,7 +3,7 @@ class CreateProjects < ActiveRecord::Migration[7.0]
     create_table :projects, id: :uuid do |t|
       t.uuid :user_id
       t.string :name
-      t.string :identifier, null: false
+      t.string :identifier
       t.string :project_type, null: false, default: 'python'
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_21_135519) do
+ActiveRecord::Schema.define(version: 2022_01_11_141057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
