de2c8e817ab0e0e97a97b82c76d0fdb624ba9a0a
Ant Barnes <ant@arayb.com>
2022-02-11 08:59:53 +0000

    Fix uniqueness index on project model

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,35 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_111_141_057) do
+ActiveRecord::Schema.define(version: 2022_02_11_085731) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'pgcrypto'
-  enable_extension 'plpgsql'
+  enable_extension "pgcrypto"
+  enable_extension "plpgsql"
 
-  create_table 'components', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
-    t.uuid 'project_id'
-    t.string 'name', null: false
-    t.string 'extension', null: false
-    t.text 'content', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['project_id'], name: 'index_components_on_project_id'
+  create_table "components", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "project_id"
+    t.string "name", null: false
+    t.string "extension", null: false
+    t.text "content", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["project_id"], name: "index_components_on_project_id"
   end
 
-  create_table 'projects', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
-    t.uuid 'user_id'
-    t.string 'name'
-    t.string 'identifier', null: false
-    t.string 'project_type', default: 'python', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['identifier'], name: 'index_projects_on_identifier'
+  create_table "projects", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "user_id"
+    t.string "name"
+    t.string "identifier", null: false
+    t.string "project_type", default: "python", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["identifier"], name: "index_projects_on_identifier", unique: true
   end
 
-  create_table 'words', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
-    t.string 'word'
-    t.index ['word'], name: 'index_words_on_word'
+  create_table "words", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "word"
+    t.index ["word"], name: "index_words_on_word"
   end
 
-  add_foreign_key 'components', 'projects'
+  add_foreign_key "components", "projects"
 end
