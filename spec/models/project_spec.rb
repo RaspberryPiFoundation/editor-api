ecd0ab0dc227b761cbe6ba653ad2ec8c46e95deb
Lois Wells <lois.wells@raspberrypi.org>
2022-02-28 17:37:45 +0000

    WIP: Adding tests for remix parents and children

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -24,4 +24,21 @@ RSpec.describe Project, type: :model do
       expect { project2.valid? }.to change(project2, :identifier)
     end
   end
+
+  describe 'relationship between parent and child projects' do
+    before(:each) do
+      @project1 = create(:project)
+      remix_params = { phrase_id: @project1.identifier, remix: { user_id: SecureRandom.uuid } }
+      @project2 = Project::Operation::CreateRemix.call(remix_params)[:project]
+      @project3 = Project::Operation::CreateRemix.call(remix_params)[:project]
+    end
+
+    it 'child can access parent project' do
+      expect(@project2.parent).to eq(@project1)
+    end
+
+    it 'parent can access child projects' do
+      expect(@project1.children).to eq([@project2])
+    end
+  end
 end
