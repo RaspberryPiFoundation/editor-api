d949d3338d7936c2036c68084621b1e0bb399d4a
Steve Gilroy <stephen.gilroy@raspberrypi.org>
2022-03-24 12:10:44 +0000

    spec update

--- spec/request/projects/images_spec.rb
+++ spec/request/projects/images_spec.rb
@@ -3,9 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Images requests', type: :request do
-  let!(:original_project) { create(:project) }
+  let!(:project) { create(:project) }
   let(:user_id) { 'e0675b6c-dc48-4cd6-8c04-0f7ac05af51a' }
+  let(:image_filename) {'test_image_1.png' }
   let(:params) { { images: [fixture_file_upload('test_image_1.png', 'image/png')] } }
+  let(:expected_json) do
+    {
+      image_list: [
+        {
+          filename: image_filename,
+          url: rails_blob_url(project.images[0])
+        }
+      ]
+    }.to_json
+  end
 
   describe 'create' do
     before do
@@ -19,10 +30,11 @@ RSpec.describe 'Images requests', type: :request do
 
       it 'returns success response' do
         expect {
-          post "/api/projects/#{original_project.identifier}/images", params: params
-        }.to change{ original_project.images.count }.by(1)
+          post "/api/projects/#{project.identifier}/images", params: params
+        }.to change{ project.images.count }.by(1)
 
         expect(response.status).to eq(200)
+        expect(response.body).to eq(expected_json)
       end
 
       it 'returns 404 response if invalid project' do
@@ -34,7 +46,7 @@ RSpec.describe 'Images requests', type: :request do
 
     context 'when auth is invalid' do
       it 'returns unauthorized' do
-        post "/api/projects/#{original_project.identifier}/images"
+        post "/api/projects/#{project.identifier}/images"
 
         expect(response.status).to eq(401)
       end
