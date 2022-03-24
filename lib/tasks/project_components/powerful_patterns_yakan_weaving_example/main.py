ff1399da3da8f20343613c1a41e592413a8997e2
Ant Barnes <ant@arayb.com>
2022-03-24 11:25:51 +0000

    update method calls in yakan weaving

--- lib/tasks/project_components/powerful_patterns_yakan_weaving_example/main.py
+++ lib/tasks/project_components/powerful_patterns_yakan_weaving_example/main.py
@@ -5,13 +5,13 @@ from math import random
 
 def motif():
   motif_size = 100
-  
+
   #Thread colours
   ORANGE = Color(254, 96, 1)
   PURPLE = Color(135, 18, 192)
   YELLOW = Color(243, 200, 19)
   BLUE = Color(83, 171, 176)
-    
+
   # Squares
   fill(ORANGE)
   rect(0, 0, motif_size/2, motif_size/2)
@@ -31,33 +31,33 @@ def motif():
   rect(50, 50, motif_size/4, motif_size/4)
 
 def rotate_motif():
-  
+
   for shape in range(5): # row of shapes
-    pushMatrix() # save settings
+    push_matrix() # save settings
     rotate(radians(45)) # turn shape 45 degrees
     motif()
-    popMatrix() # go back to saved settings
+    pop_matrix() # go back to saved settings
     translate(motif_width, 0) # move horizontally
 
 def setup():
   size(400, 400)
   background(250, 5, 94) # pink
   no_stroke()
-  print('This is 🇵🇭 Yakan weaving ') 
-  
+  print('This is 🇵🇭 Yakan weaving ')
+
 def draw():
-  
+
   global motif_width
-  motif_width = 150 
-  
-  translate(-motif_width/2, -motif_width/2) # to start with half motifs 
-  
+  motif_width = 150
+
+  translate(-motif_width/2, -motif_width/2) # to start with half motifs
+
   if frame_count < 20: # maximum rows
     for row in range(frame_count):
       rotate_motif()
       if row / 2 == 0: # to offset pattern on next row
-        translate(-motif_width * 5 + 75, 80) 
-      else:  
-        translate(-motif_width * 5 - 75, 80) 
-        
+        translate(-motif_width * 5 + 75, 80)
+      else:
+        translate(-motif_width * 5 - 75, 80)
+
 run(frame_rate=3)
