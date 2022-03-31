110bc6ef5cffbc99e91eb60b4c58966ec9e51c99
Lois Wells <lois.wells@raspberrypi.org>
2022-03-31 13:41:25 +0000

    codebreaker starter and example projects

---
+++ lib/tasks/project_components/codebreaker_example/frequency.py
@@ -0,0 +1,31 @@
+# Frequency distribution of letters in multiple languages
+# Provided as part of the starter
+
+english = {
+  'a': 8.20,
+  'b': 1.50,
+  'c': 2.80,
+  'd': 4.30,
+  'e': 13,
+  'f': 2.20,
+  'g': 2,
+  'h': 6.10,
+  'i': 7,
+  'j': 0.15,
+  'k': 0.77,
+  'l': 4,
+  'm': 2.40,
+  'n': 6.70,
+  'o': 7.50,
+  'p': 1.90,
+  'q': 0.10,
+  'r': 6,
+  's': 6.30,
+  't': 9.10,
+  'u': 2.80,
+  'v': 0.98,
+  'w': 2.40,
+  'x': 0.15,
+  'y': 2,
+  'z': 0.07
+}
---
+++ lib/tasks/project_components/codebreaker_example/main.py
@@ -0,0 +1,94 @@
+##!/bin/python3 
+from pygal import Bar
+from frequency import english
+
+# Set up data structures 
+alphabet = list(' abcdefghijklmnopqrstuvwxyz ') # List from a string
+code = {}
+
+# Create the atbash code by reversing the alphabet
+def create_code():
+  backwards = list(reversed(alphabet)) # Reversing a list
+  
+  for i in range(len(alphabet)): # Getting length of a list
+    code[alphabet[i]] = backwards[i] # Populate the code dictionary with a letter of the alphabet and its encoded letter
+  
+  #print(code)
+
+# Calculate the frequency of all letters in a piece of text
+def frequency(text):
+  text = list(text.lower()) # Lowercase the message and make it a list
+  
+  freq = {} # Create a dict of every letter, with a count of 0
+  for letter in alphabet:
+    freq[letter] = 0
+  
+  total_letters = len(text) # Count the letters in the message
+  
+  for letter in text:
+    if letter in freq: 
+      freq[letter] += 1
+  
+  for letter in freq: # Convert from counts to percentages
+    freq[letter] = freq[letter] / total_letters * 100
+  
+  return freq
+
+# Make frequency chart
+def make_chart(text, language):
+  chart = Bar()
+  chart.title = 'Frequency analysis'
+  chart.x_labels = text.keys() # First explicit use of keys
+  chart.add('Target message', text.values()) # First explicit use of values
+  chart.add('Language', language.values())
+  
+  chart.render()
+
+
+# Encode/decode a piece of text — atbash is symetrical
+def atbash(text):
+  text = text.lower() # Converting text to lowercase
+  output = ''
+  
+  for letter in text: 
+    if letter in code: 
+      output += code[letter] # Populate output with the encoded/decoded message using the dictionary
+  
+  return output # Return the encoded/decoded message
+
+
+# Fetch and return text from a file
+def get_text(filename):
+    with open(filename) as f:
+      text = f.read().replace('\n','') # Need to strip the newline characters
+    
+    return text
+
+# Create a text-based menu system  
+def menu():
+  choice = '' # Start with a wrong answer for choice.
+  
+  while choice != 'c' and choice != 'f': # Keep asking the user for the right answer
+    choice = input('Please enter c to encode/decode text, or f to perform frequency analysis:' )
+  
+  if choice == 'c':
+    print('Running your message through the cypher…')
+    message = get_text('longer.txt') # Take input from a file 
+    code = atbash(message)
+    print(code)
+
+  elif choice == 'f':
+    print('Analysing message…')
+    message = get_text('longer.txt') # Take input from the same file. We have a 'longer.txt' or similar containing cyphertext we know to perform reasonably well for frequency analysis
+    message_freq = frequency(message) # Get the frequency of the letters in the message, as %
+    # print(message_freq)
+    lang_freq = english # Import the English frequency dictionary
+    make_chart(message_freq, lang_freq) # Call the function to make a chart
+    
+# Start up
+def main():
+  create_code()
+  menu()
+
+  
+main()
