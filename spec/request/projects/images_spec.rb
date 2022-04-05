c2d065cff225d3614512acb57527850aa6d28e70
Steve Gilroy <stephen.gilroy@raspberrypi.org>
2022-04-05 11:27:57 +0100

    update tests and distinguish forbidden from unauthorized

--- spec/request/projects/images_spec.rb
+++ spec/request/projects/images_spec.rb
@@ -3,8 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe 'Images requests', type: :request do
-  let!(:project) { create(:project) }
   let(:user_id) { 'e0675b6c-dc48-4cd6-8c04-0f7ac05af51a' }
+  let(:project) { create(:project, user_id: user_id) }
   let(:image_filename) { 'test_image_1.png' }
   let(:params) { { images: [fixture_file_upload(image_filename, 'image/png')] } }
   let(:expected_json) do
@@ -21,7 +21,7 @@ RSpec.describe 'Images requests', type: :request do
   describe 'create' do
     context 'when auth is correct' do
       before do
-        mock_oauth_user
+        mock_oauth_user(user_id)
       end
 
       it 'attaches file to project' do
@@ -44,6 +44,18 @@ RSpec.describe 'Images requests', type: :request do
         post '/api/projects/no-such-project/images'
 
         expect(response.status).to eq(404)
+      end
+    end
+
+    context 'when authed user is not creator' do
+  
+      before do
+        mock_oauth_user
+      end
+  
+      it 'returns forbidden response' do
+        post "/api/projects/#{project.identifier}/images", params: params
+        expect(response.status).to eq(403)
       end
     end
 
