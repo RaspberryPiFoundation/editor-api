930668d1374d12453d99700674f72b86d4ababbc
Ant Barnes <ant@arayb.com>
2022-04-14 14:50:22 +0100

    rename project children to remixes

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe Project, type: :model do
   describe 'associations' do
     it { is_expected.to have_many(:components) }
-    it { is_expected.to have_many(:children) }
+    it { is_expected.to have_many(:remixes).dependent(:nullify) }
     it { is_expected.to belong_to(:parent).optional(true) }
     it { is_expected.to have_many_attached(:images) }
 
