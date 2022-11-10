e150a8d22063c06655399cdb58c8a5f7fc3ae61c
Liz Smart <liz.smart@raspberrypi.org>
2022-11-10 11:13:43 +0000

    Update main.py
    Updating starter code

--- lib/tasks/project_components/target_practice_starter/main.py
+++ lib/tasks/project_components/target_practice_starter/main.py
@@ -1,25 +1,20 @@
-#!/bin/python3
-
 # Import library code
 from p5 import *
-from math import *
 from random import randint
 
 # The mouse_pressed function goes here
-
+    
 # The shoot_arrow function goes here
 
 def setup():
 # Setup your game here
-  size(400, 400) # width and height
+    size(400, 400) # width and height
+
 
 def draw():
 # Things to do in every frame
-  sky = Color(92, 204, 206) # Red = 92, Green = 204, Blue = 206
-  grass = Color(149, 212, 122)
-  wood = Color(145, 96, 51)
-  outer = Color(0, 120, 180)
-
-
+  fill('cyan')
+  rect(0, 0, 400, 250) # Sky
+  
 # Keep this to run your code
 run(frame_rate=2)
