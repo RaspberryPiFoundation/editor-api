201efff00491c7e85ee9fe8f160e0ba4bac08b7a
Lois Wells <lois.wells@raspberrypi.org>
2022-12-01 11:51:57 +0000

    adding project show tests for new abilities

--- spec/request/projects/show_spec.rb
+++ spec/request/projects/show_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe 'Project show requests', type: :request do
   let!(:project) { create(:project) }
-  let(:expected_json) do
+  let(:project_json) do
     {
       identifier: project.identifier,
       project_type: 'python',
@@ -15,25 +15,103 @@ RSpec.describe 'Project show requests', type: :request do
     }.to_json
   end
 
-  it 'returns success response' do
-    get "/api/projects/#{project.identifier}"
+  context 'when user is logged in' do
+    before do
+      mock_oauth_user(project.user_id)
+    end
 
-    expect(response).to have_http_status(:ok)
+    context 'loading own project' do
+      it 'returns success response' do
+        get "/api/projects/#{project.identifier}"
+    
+        expect(response).to have_http_status(:ok)
+      end
+    
+      it 'returns json' do
+        get "/api/projects/#{project.identifier}"
+        expect(response.content_type).to eq('application/json; charset=utf-8')
+      end
+    
+      it 'returns the project json' do
+        get "/api/projects/#{project.identifier}"
+        expect(response.body).to eq(project_json)
+      end
+    end
+
+    context 'loading another user\'s project' do
+      let!(:another_project) { create(:project) }
+      let(:another_project_json) do
+        {
+          identifier: another_project.identifier,
+          project_type: 'python',
+          name: another_project.name,
+          user_id: another_project.user_id,
+          components: [],
+          image_list: []
+        }.to_json
+      end
+    
+      it 'returns forbidden response' do
+        get "/api/projects/#{another_project.identifier}"
+    
+        expect(response).to have_http_status(:forbidden)
+      end
+    
+      it 'does not return the project json' do 
+        get "/api/projects/#{another_project.identifier}"
+        expect(response.body).not_to include(another_project_json)
+      end
+
+    end
   end
 
-  it 'returns json' do
-    get "/api/projects/#{project.identifier}"
-    expect(response.content_type).to eq('application/json; charset=utf-8')
-  end
+  context 'when user is not logged in' do
+    context 'loading a starter project' do
+      let!(:starter_project) { create(:project, user_id: nil) }
+      let(:starter_project_json) do
+        {
+          identifier: starter_project.identifier,
+          project_type: 'python',
+          name: starter_project.name,
+          user_id: starter_project.user_id,
+          components: [],
+          image_list: []
+        }.to_json
+      end
 
-  it 'returns the project json' do
-    get "/api/projects/#{project.identifier}"
-    expect(response.body).to eq(expected_json)
-  end
+      it 'returns success response' do
+        get "/api/projects/#{starter_project.identifier}"
 
-  it 'returns 404 response if invalid project' do
-    get '/api/projects/no-such-project'
+        expect(response).to have_http_status(:ok)
+      end
 
-    expect(response).to have_http_status(:not_found)
+      it 'returns json' do
+        get "/api/projects/#{starter_project.identifier}"
+        expect(response.content_type).to eq('application/json; charset=utf-8')
+      end
+
+      it 'returns the project json' do
+        get "/api/projects/#{starter_project.identifier}"
+        expect(response.body).to eq(starter_project_json)
+      end
+
+      it 'returns 404 response if invalid project' do
+        get '/api/projects/no-such-project'
+        expect(response).to have_http_status(:not_found)
+      end
+    end
+
+    context 'loading an owned project' do 
+      it 'returns forbidden response' do
+        get "/api/projects/#{project.identifier}"
+    
+        expect(response).to have_http_status(:forbidden)
+      end
+    
+      it 'does not return the project json' do 
+        get "/api/projects/#{project.identifier}"
+        expect(response.body).not_to include(project_json)
+      end
+    end
   end
 end
