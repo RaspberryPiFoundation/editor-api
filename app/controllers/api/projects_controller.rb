3ae3bc07b11b7edb6cbc0edb7c58fca289d88599
Izzy Smillie <isabelle.smillie@raspberrypi.org>
2023-01-10 11:30:09 +0000

    Add pagination to projects index

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -9,7 +9,8 @@ module Api
     skip_load_resource only: :create
 
     def index
-      render :index, formats: [:json]
+      paginated_projects = projects.page(params[:page]).per(8)
+      render index: paginated_projects, formats: [:json]
     end
 
     def show
