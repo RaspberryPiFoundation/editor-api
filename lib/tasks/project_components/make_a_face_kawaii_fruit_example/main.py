8cce8c9f865556b5b7257d7036baaf1c4ac9c519
chrisruk <christopher.richardson@raspberrypi.org>
2023-02-08 17:05:34 +0000

    Changes for make a face kawaii fruit example for thonny

--- lib/tasks/project_components/make_a_face_kawaii_fruit_example/main.py
+++ lib/tasks/project_components/make_a_face_kawaii_fruit_example/main.py
@@ -1,42 +1,42 @@
 #!/bin/python3
 
-from p5 import *
+import py5
 
 def setup():
-  size(400, 400)
-  no_stroke()
+  py5.size(400, 400)
+  py5.no_stroke()
   
 
 def draw():
-  background(255)
-  orange = Color(255, 165, 0)
-  brown = Color(200, 120, 0)
-  green = Color(100, 155, 0)
-  fill(orange)
-  ellipse(200, 200, 200, 190)
-  fill(0)
+  py5.background(255)
+  orange = py5.color(255, 165, 0)
+  brown = py5.color(200, 120, 0)
+  green = py5.color(100, 155, 0)
+  py5.fill(orange)
+  py5.ellipse(200, 200, 200, 190)
+  py5.fill(0)
   # Eyes
-  ellipse(160, 220, 30, 30)
-  ellipse(240, 220, 30, 30)
-  fill(255)
-  ellipse(165, 215, 10, 10)
-  ellipse(245, 215, 10, 10)
+  py5.ellipse(160, 220, 30, 30)
+  py5.ellipse(240, 220, 30, 30)
+  py5.fill(255)
+  py5.ellipse(165, 215, 10, 10)
+  py5.ellipse(245, 215, 10, 10)
   # Mouth
-  fill(0)
-  ellipse(200, 240, 15, 15)
-  fill(orange)
-  ellipse(200, 235, 15, 15)
+  py5.fill(0)
+  py5.ellipse(200, 240, 15, 15)
+  py5.fill(orange)
+  py5.ellipse(200, 235, 15, 15)
   # Highlights
-  fill(255, 70)
-  ellipse(170, 150, 35, 35)
-  ellipse(150, 160, 25, 25)
+  py5.fill(255, 70)
+  py5.ellipse(170, 150, 35, 35)
+  py5.ellipse(150, 160, 25, 25)
   # stalk
-  fill(brown)
-  triangle(200, 130, 220, 60, 240, 60);
-  fill(green)
-  translate(180, 85)
-  rotate(radians(45))
-  ellipse(0, 0, 75, 35)
+  py5.fill(brown)
+  py5.triangle(200, 130, 220, 60, 240, 60);
+  py5.fill(green)
+  py5.translate(180, 85)
+  py5.rotate(py5.radians(45))
+  py5.ellipse(0, 0, 75, 35)
 
-run()
+py5.run_sketch()
 
