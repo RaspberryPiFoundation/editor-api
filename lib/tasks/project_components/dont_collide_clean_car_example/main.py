4aa23259318f617d1d5b40f48b16d2e19544dc23
chrisruk <christopher.richardson@raspberrypi.org>
2023-02-09 14:25:27 +0000

    Changes for dont collide clean car example for thonny

--- lib/tasks/project_components/dont_collide_clean_car_example/main.py
+++ lib/tasks/project_components/dont_collide_clean_car_example/main.py
@@ -1,7 +1,7 @@
 #!/bin/python3
 
 # Import library code
-from p5 import *
+import py5
 from random import randint, seed
 
 level = 1
@@ -14,32 +14,32 @@ def draw_obstacles():
   
   seed(123456789)
   
-  if frame_count % width == width - 1 and level < 10:
+  if py5.frame_count % py5.width == py5.width - 1 and level < 10:
     level += 1
     print('You reached level', level)
     
   for i in range(6 + level):
-    ob_x = randint(0, width) - (frame_count * level)
-    ob_y = randint(0, height) 
-    ob_x %= width # wrap around
-    text('💩', ob_x, ob_y)
+    ob_x = randint(0, py5.width) - (py5.frame_count * level)
+    ob_y = randint(0, py5.height) 
+    ob_x %= py5.width # wrap around
+    py5.text('💩', ob_x, ob_y)
     
 # The draw_player function goes here
 def draw_player():
   
   global score, level
   
-  player_x = int(width * 0.2)
-  player_y = mouse_y
+  player_x = int(py5.width * 0.2)
+  player_y = py5.mouse_y
   
-  collide = get(player_x + 50, player_y + 15)
-  collide2 = get(player_x + 50, player_y - 15)
-  collide3 = get(player_x, player_y + 15)
-  collide4 = get(player_x, player_y - 15)
-  collide5 = get(player_x - 50, player_y + 15)
-  collide6 = get(player_x - 50, player_y - 15)
+  collide = py5.get(player_x + 50, player_y + 15)
+  collide2 = py5.get(player_x + 50, player_y - 15)
+  collide3 = py5.get(player_x, player_y + 15)
+  collide4 = py5.get(player_x, player_y - 15)
+  collide5 = py5.get(player_x - 50, player_y + 15)
+  collide6 = py5.get(player_x - 50, player_y - 15)
   
-  if player_y > height - 18: # Off the bottom of the screen
+  if player_y > py5.height - 18: # Off the bottom of the screen
     collide = safe
     collide3 = safe
     collide5 = safe
@@ -50,10 +50,10 @@ def draw_player():
     collide6 = safe
     
   if collide == safe and collide2 == safe and collide3 == safe and collide4 == safe:
-    image(car, player_x, player_y, 100, 31)
+    py5.image(car, player_x, player_y, 100, 31)
     score += level
   else:
-    text('💥', player_x, player_y)
+    py5.text('💥', player_x, player_y)
     level = 0
     
   
@@ -61,26 +61,28 @@ def setup():
   # Setup your animation here
   global car
   
-  size(400, 400)
-  car = load_image('car.png')
-  image_mode(CENTER)
+  py5.size(400, 400)
+  font = py5.create_font("Monaco", 32)
+  py5.text_font(font)
+  car = py5.load_image('car.png')
+  py5.image_mode(py5.CENTER)
   
   
 def draw():
   # Things to do in every frame
   global score, safe, level
-  safe = Color(128)
+  safe = py5.color(128)
   
   if level > 0:
-    background(safe)
-    fill(255)
-    text_size(16)
-    text_align(RIGHT, TOP)
-    text('Score', width * 0.45, 10, width * 0.5, 20)
-    text(str(score), width * 0.45, 25, width * 0.5, 20)
-    text_size(20)
-    text_align(CENTER, TOP) # position around the centre, top
+    py5.background(safe)
+    py5.fill(255)
+    py5.text_size(16)
+    py5.text_align(py5.RIGHT, py5.TOP)
+    py5.text('Score', py5.width * 0.45, 10, py5.width * 0.5, 20)
+    py5.text(str(score), py5.width * 0.45, 25, py5.width * 0.5, 20)
+    py5.text_size(20)
+    py5.text_align(py5.CENTER, py5.TOP) # position around the centre, top
     draw_obstacles()
     draw_player()
   
-run()
+py5.run_sketch()
