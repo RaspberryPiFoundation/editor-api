b4462a6524c616497513538d990216db0b9fc6ad
Patrick J. Cherry <patch@dominoid.net>
2023-02-16 16:26:24 +0000

    Move request specs to the correct directory
    This allows rspec to correctly infer the test type

--- spec/requests/projects/create_spec.rb
+++ spec/requests/projects/create_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Create project requests', type: :request do
+RSpec.describe 'Create project requests' do
   let(:user_id) { 'e0675b6c-dc48-4cd6-8c04-0f7ac05af51a' }
   let(:project) { create(:project, user_id:) }
 
