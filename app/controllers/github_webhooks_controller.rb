c148319374e87a051ef914bb1c151bb975a1677f
Lois Wells <lois.wells@raspberrypi.org>
2023-02-16 16:38:27 +0000

    fixing test for webhook controller

--- app/controllers/github_webhooks_controller.rb
+++ app/controllers/github_webhooks_controller.rb
@@ -5,7 +5,6 @@ class GithubWebhooksController < ActionController::API
 
   def github_push(payload)
     UploadJob.perform_later if payload[:ref] == ENV.fetch('GITHUB_WEBHOOK_REF') && edited_code?(payload)
-    head :ok
   end
 
   private
@@ -17,12 +16,6 @@ class GithubWebhooksController < ActionController::API
   def edited_code?(payload)
     commits = payload[:commits]
     modified_paths = commits.map { |commit| commit[:added] | commit[:modified] | commit[:removed] }.flatten
-
-    modified_code_paths = modified_paths.filter do |path|
-      path_components = path.split('/')
-      path_components[0] == 'en' && path_components[1] == 'code'
-    end
-
-    modified_code_paths.length.positive?
+    modified_paths.count { |path| path.start_with?('en/code') }.positive?
   end
 end
--- spec/request/github_webhooks/github_push_spec.rb
+++ spec/request/github_webhooks/github_push_spec.rb
@@ -6,25 +6,31 @@ RSpec.describe GithubWebhooksController, type: :request do
   # let(:user_id) { 'e0675b6c-dc48-4cd6-8c04-0f7ac05af51a' }
   # let(:project) { create(:project, user_id:) }
 
-  describe 'github_push' do
-    # context 'when auth is correct' do
-      # before do
-        # mock_oauth_user(user_id)
-
-      #   response = OperationResponse.new
-      #   response[:project] = project
-      #   allow(Project::Create).to receive(:call).and_return(response)
-      # end
-      params = {
-        ref: '/branches/main',
-        commits: []
-      }
-
-      it 'returns success' do
-        post '/github_webhooks', params: params, headers: {'x-hub-signature-256': ENV.fetch('GITHUB_WEBHOOK_SECRET')}
-        expect(response).to have_http_status(:ok)
-      end
-    # end
-
+  around do |example|
+    ClimateControl.modify GITHUB_WEBHOOK_SECRET: 'secret', GITHUB_WEBHOOK_REF: 'branches/whatever' do
+      example.run
     end
+  end
+
+  describe 'github_push' do
+    let(:params) {{
+      ref: '/branches/main',
+      commits: []
+    }}
+
+    let(:headers) {
+      {
+        'X-Hub-Signature-256': "sha256=#{OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), ENV.fetch('GITHUB_WEBHOOK_SECRET'), params.to_json)}",
+        'X-GitHub-Event': 'push'
+      }
+    }
+
+    before do
+      post '/github_webhooks', params: params.to_json, headers: headers
+    end
+
+    it 'returns success' do
+      expect(response).to have_http_status(:ok)
+    end
+  end
 end
