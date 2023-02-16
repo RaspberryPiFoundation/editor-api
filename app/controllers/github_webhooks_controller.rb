c043c688fc37b3ab55e12558b7aecfaeaf4f6860
Lois Wells <lois.wells@raspberrypi.org>
2023-02-16 14:47:21 +0000

    Checking which files were modifed

--- app/controllers/github_webhooks_controller.rb
+++ app/controllers/github_webhooks_controller.rb
@@ -4,7 +4,7 @@ class GithubWebhooksController < ActionController::API
   include GithubWebhook::Processor
 
   def github_push(payload)
-    UploadJob.perform_later if payload['ref'] == ENV.fetch('GITHUB_WEBHOOK_REF')
+    UploadJob.perform_later if payload[:ref] == ENV.fetch('GITHUB_WEBHOOK_REF') && edited_code?(payload)
     head :ok
   end
 
@@ -12,5 +12,17 @@ class GithubWebhooksController < ActionController::API
 
   def webhook_secret(_payload)
     ENV.fetch('GITHUB_WEBHOOK_SECRET')
+  end
+
+  def edited_code?(payload)
+    commits = payload[:commits]
+    modified_paths = commits.map { |commit| commit[:added] | commit[:modified] | commit[:removed] }.flatten
+
+    modified_code_paths = modified_paths.filter do |path|
+      path_components = path.split('/')
+      path_components[0] == 'en' && path_components[1] == 'code'
+    end
+
+    modified_code_paths.length.positive?
   end
 end
