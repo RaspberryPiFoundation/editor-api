e15106c7d6bb46ac928261399c8fff9b8c9f5920
Lois Wells <lois.wells@raspberrypi.org>
2023-02-17 09:29:55 +0000

    rubocop fixes

--- spec/requests/github_webhooks/github_webhooks_controller_push_spec.rb
+++ spec/requests/github_webhooks/github_webhooks_controller_push_spec.rb
@@ -3,7 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe GithubWebhooksController do
-
   around do |example|
     ClimateControl.modify GITHUB_WEBHOOK_SECRET: 'secret', GITHUB_WEBHOOK_REF: 'branches/whatever' do
       example.run
@@ -11,29 +10,31 @@ RSpec.describe GithubWebhooksController do
   end
 
   describe 'github_push' do
-    let(:params) {{
-      ref: ref,
-      commits: commits
-    }}
+    let(:params) do
+      {
+        ref:,
+        commits:
+      }
+    end
 
-    let(:headers) {
+    let(:headers) do
       {
         'X-Hub-Signature-256': "sha256=#{OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), ENV.fetch('GITHUB_WEBHOOK_SECRET'), params.to_json)}",
         'X-GitHub-Event': 'push',
         'Content-Type': 'application/json'
       }
-    }
+    end
 
-    before(:example) do
+    before do
       allow(UploadJob).to receive(:perform_later)
-      post '/github_webhooks', env: { 'RAW_POST_DATA': params.to_json }, headers: headers
+      post '/github_webhooks', env: { RAW_POST_DATA: params.to_json }, headers:
     end
 
     context 'when webhook ref matches branch of interest' do
       let(:ref) { 'branches/whatever' }
 
       context 'when code has been added' do
-        let(:commits) { [ { added: [], modified: [], removed: ['en/code/project1/main.py'] } ] }
+        let(:commits) { [{ added: ['en/code/project1/main.py'], modified: [], removed: [] }] }
 
         it 'schedules the upload job' do
           expect(UploadJob).to have_received(:perform_later)
@@ -41,7 +42,7 @@ RSpec.describe GithubWebhooksController do
       end
 
       context 'when code has been modified' do
-        let(:commits) { [ { added: [], modified: [ 'en/code/project1/main.py' ], removed: [] } ] }
+        let(:commits) { [{ added: [], modified: ['en/code/project1/main.py'], removed: [] }] }
 
         it 'schedules the upload job' do
           expect(UploadJob).to have_received(:perform_later)
@@ -49,7 +50,7 @@ RSpec.describe GithubWebhooksController do
       end
 
       context 'when code has been removed' do
-        let(:commits) { [ { added: [], modified: [], removed: ['en/code/project1/main.py'] } ] }
+        let(:commits) { [{ added: [], modified: [], removed: ['en/code/project1/main.py'] }] }
 
         it 'schedules the upload job' do
           expect(UploadJob).to have_received(:perform_later)
@@ -57,7 +58,7 @@ RSpec.describe GithubWebhooksController do
       end
 
       context 'when code has not been changed' do
-        let(:commits) { [ { added: ['en/step2.md'], modified: [ 'en/step1.md' ], removed: ['en/step0.md'] } ] }
+        let(:commits) { [{ added: ['en/step2.md'], modified: ['en/step1.md'], removed: ['en/step0.md'] }] }
 
         it 'does not schedule the upload job' do
           expect(UploadJob).not_to have_received(:perform_later)
@@ -67,7 +68,7 @@ RSpec.describe GithubWebhooksController do
 
     context 'when webhook ref does not match branch of interest' do
       let(:ref) { 'branches/master' }
-      let(:commits) { [ { added: [], modified: [ 'en/code/project1/main.py' ], removed: [] } ] }
+      let(:commits) { [{ added: [], modified: ['en/code/project1/main.py'], removed: [] }] }
 
       it 'does not schedule the upload job' do
         expect(UploadJob).not_to have_received(:perform_later)
