947b1693ffa3e91f2de0d8b5bfaadddd56381089
Patrick J. Cherry <patch@dominoid.net>
2023-02-17 09:14:18 +0000

    Add missed controller :doh:

---
+++ app/controllers/graphql_controller.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+class GraphqlController < ApplicationController
+  include AuthenticationConcern
+  include ActiveStorage::SetCurrent if Rails.env.development?
+
+  skip_before_action :verify_authenticity_token
+
+  # If accessing from outside this domain, nullify the session
+  # This allows for outside API access while preventing CSRF attacks,
+  # but you'll have to authenticate your user separately
+  # protect_from_forgery with: :null_session
+
+  def execute
+    result = EditorApiSchema.execute(query, variables:, context:, operation_name:)
+    render json: result
+  rescue StandardError => e
+    raise e unless Rails.env.development?
+
+    handle_error_in_development(e)
+  end
+
+  private
+
+  def query
+    params[:query]
+  end
+
+  def operation_name
+    params[:operationName]
+  end
+
+  def context
+    @context ||= { current_user_id:, current_ability: Ability.new(current_user_id) }
+  end
+
+  # Handle variables in form data, JSON body, or a blank value
+  def variables
+    case params[:variables]
+    when String
+      if variables_param.present?
+        JSON.parse(variables_param) || {}
+      else
+        {}
+      end
+    when Hash
+      variables_param
+    when ActionController::Parameters
+      variables_param.to_unsafe_hash # GraphQL-Ruby will validate name and type of incoming variables.
+    when nil
+      {}
+    else
+      raise ArgumentError, "Unexpected parameter: #{variables_param}"
+    end
+  end
+
+  def handle_error_in_development(error)
+    logger.error error.message
+    logger.error error.backtrace.join("\n")
+
+    render json: { errors: [{ message: error.message, backtrace: error.backtrace }], data: {} },
+           status: :internal_server_error
+  end
+end
