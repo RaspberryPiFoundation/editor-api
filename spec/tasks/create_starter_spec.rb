3bd061403fda8264fa4bc5f015e9842988b3c674
Lois Wells <lois.wells@raspberrypi.org>
2023-02-17 10:46:37 +0000

    removing test

--- spec/tasks/create_starter_spec.rb
+++ spec/tasks/create_starter_spec.rb
@@ -15,13 +15,4 @@ describe 'projects:create_starter', type: :task do
     allow(File).to receive(:read).and_return('print("hello")')
     expect { Rake::Task['projects:create_starter'].invoke }.not_to raise_error
   end
-
-  it 'calls the ProjectImporter' do
-    expected_config = { components: [], identifier: 'my-amazing-project', images: [], name: 'My amazing project', type: 'python' }
-
-    allow(ProjectImporter).to receive(:new)
-    ProjectImporter.new(name: project_config['NAME'], identifier: project_config['IDENTIFIER'],
-                        type: project_config['TYPE'] ||= 'python', components: [], images: [])
-    expect(ProjectImporter).to have_received(:new).with(expected_config)
-  end
 end
