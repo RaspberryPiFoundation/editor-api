fe23a8d85b9b246e5f108444846bc5282593bca1
Lois Wells <lois.wells@raspberrypi.org>
2023-02-22 12:02:02 +0000

    fixing rubocop

--- spec/jobs/upload_job_spec.rb
+++ spec/jobs/upload_job_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe UploadJob do
@@ -6,46 +8,53 @@ RSpec.describe UploadJob do
       example.run
     end
   end
-  ActiveJob::Base.queue_adapter = :test
-  let(:graphql_response) {
-    GraphQL::Client::Response.new(raw_response, data: UploadJob::ProjectContentQuery.new(raw_response["data"], GraphQL::Client::Errors.new()))
-  }
 
-  let(:raw_response) {
+  ActiveJob::Base.queue_adapter = :test
+  let(:graphql_response) do
+    GraphQL::Client::Response.new(raw_response, data: UploadJob::ProjectContentQuery.new(raw_response['data'], GraphQL::Client::Errors.new))
+  end
+  let(:payload) do
+    { repository: { name: 'my-amazing-repo', owner: { name: 'me' } } }
+  end
+  let(:variables) do
+    { repository: 'my-amazing-repo', owner: 'me', expression: "#{ENV.fetch('GITHUB_WEBHOOK_REF')}:en/code" }
+  end
+
+  let(:raw_response) do
     {
       data: {
         repository: {
           object: {
-            __typename: "Tree",
+            __typename: 'Tree',
             entries: [
               {
-                name: "dont-collide-starter",
+                name: 'dont-collide-starter',
                 object: {
-                  __typename: "Tree",
+                  __typename: 'Tree',
                   entries: [
                     {
-                      name: "astronaut1.png",
-                      extension: ".png",
+                      name: 'astronaut1.png',
+                      extension: '.png',
                       object: {
-                        __typename: "Blob",
+                        __typename: 'Blob',
                         text: nil,
                         isBinary: true
                       }
                     },
                     {
-                      name: "main.py",
-                      extension: ".py",
+                      name: 'main.py',
+                      extension: '.py',
                       object: {
-                        __typename: "Blob",
+                        __typename: 'Blob',
                         text: "#!/bin/python3\n\nfrom p5 import *\nfrom random import randint, seed\n\n# Include global variables here\n\n\ndef setup():\n# Put code to run once here\n\n\ndef draw():\n# Put code to run every frame here\n\n  \n# Keep this to run your code\nrun()\n",
                         isBinary: false
                       }
                     },
                     {
-                      name: "project_config.yml",
-                      extension: ".yml",
+                      name: 'project_config.yml',
+                      extension: '.yml',
                       object: {
-                        __typename: "Blob",
+                        __typename: 'Blob',
                         text: "name: \"Don't Collide!\"\nidentifier: \"dont-collide-starter\"\ntype: \"python\"\n",
                         isBinary: true
                       }
@@ -58,39 +67,10 @@ RSpec.describe UploadJob do
         }
       }
     }.deep_stringify_keys
-  }
-
-  let(:image_url) {
-    "https://github.com/me/my-amazing-repo/raw/branches/whatever/en/code/dont-collide-starter/astronaut1.png"
-  }
-  
-  before do
-    allow(GitHub::Client).to receive(:query).and_return(graphql_response)
-    stub_request(:get, image_url).to_return(status: 200, body: '', headers: {})
-    allow(ProjectImporter).to receive(:new).and_call_original
   end
 
-  let(:payload) {
-    { repository: { name: 'my-amazing-repo', owner: { name: 'me' } } }
-  }
-
-  let(:variables) {
-    { repository: 'my-amazing-repo', owner: 'me', expression: "#{ ENV.fetch('GITHUB_WEBHOOK_REF') }:en/code" }
-  }
-
-  it 'enqueues the job' do
-    expect{ UploadJob.perform_later(payload) }.to enqueue_job
-  end
-
-  it 'requests data from GitHub' do
-    UploadJob.perform_now(payload)
-    expect(GitHub::Client).to have_received(:query).with(UploadJob::ProjectContentQuery, variables: variables)
-  end
-
-  it 'imports the project in the correct format' do
-    UploadJob.perform_now(payload)
-    expect(ProjectImporter).to have_received(:new)
-    .with(
+  let(:project_importer_params) do
+    {
       name: "Don't Collide!",
       identifier: 'dont-collide-starter',
       type: 'python',
@@ -108,15 +88,36 @@ RSpec.describe UploadJob do
           io: instance_of(StringIO)
         }
       ]
-    )
+    }
+  end
+
+  before do
+    allow(GitHub::Client).to receive(:query).and_return(graphql_response)
+    stub_request(:get, 'https://github.com/me/my-amazing-repo/raw/branches/whatever/en/code/dont-collide-starter/astronaut1.png').to_return(status: 200, body: '', headers: {})
+    allow(ProjectImporter).to receive(:new).and_call_original
+  end
+
+  it 'enqueues the job' do
+    expect { described_class.perform_later(payload) }.to enqueue_job
+  end
+
+  it 'requests data from GitHub' do
+    described_class.perform_now(payload)
+    expect(GitHub::Client).to have_received(:query).with(UploadJob::ProjectContentQuery, variables:)
+  end
+
+  it 'imports the project in the correct format' do
+    described_class.perform_now(payload)
+    expect(ProjectImporter).to have_received(:new)
+      .with(**project_importer_params)
   end
 
   it 'requests the image from the correct URL' do
-    UploadJob.perform_now(payload)
-    expect(WebMock).to have_requested(:get, image_url).once
+    described_class.perform_now(payload)
+    expect(WebMock).to have_requested(:get, 'https://github.com/me/my-amazing-repo/raw/branches/whatever/en/code/dont-collide-starter/astronaut1.png').once
   end
 
   it 'saves the project to the database' do
-    expect{ UploadJob.perform_now(payload) }.to change(Project, :count).by(1)
+    expect { described_class.perform_now(payload) }.to change(Project, :count).by(1)
   end
 end
