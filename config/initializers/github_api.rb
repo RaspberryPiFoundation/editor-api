208d156136976be46ae3620b8ce89dce67125f04
Patrick J. Cherry <patch@dominoid.net>
2023-02-24 13:17:40 +0000

    Remove underscores

--- config/initializers/github_api.rb
+++ config/initializers/github_api.rb
@@ -4,8 +4,8 @@ require 'github_api'
 require 'graphql/client/log_subscriber'
 
 Rails.application.configure do
-  config.git_hub_api = ActiveSupport::OrderedOptions.new
-  config.git_hub_api.client = GithubApi::Client
+  config.github_api = ActiveSupport::OrderedOptions.new
+  config.github_api.client = GithubApi::Client
 
-  GraphQL::Client::LogSubscriber.attach_to :git_hub_api
+  GraphQL::Client::LogSubscriber.attach_to :github_api
 end
