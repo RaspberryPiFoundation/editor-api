b3649e9b4d2f1f0061117d09085373e10aae682d
Lois Wells <lois.wells@raspberrypi.org>
2023-03-07 17:37:19 +0000

    limit identifier to the same user

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,8 +4,9 @@ require 'phrase_identifier'
 
 class Project < ApplicationRecord
   before_validation :check_unique_not_null, on: :create
-  validates :identifier, presence: true, uniqueness: { scope: :locale }, if: :locale
-  validates :identifier, presence: true, uniqueness: true, unless: :locale
+  validates :identifier, presence: true, uniqueness: { scope: :locale }
+  validate :identifier_cannot_be_taken_by_another_user
+  validates :locale, presence: true, unless: :user_id
   belongs_to :parent, class_name: 'Project', foreign_key: 'remixed_from_id', optional: true, inverse_of: :remixes
   has_many :components, -> { order(default: :desc, name: :asc) }, dependent: :destroy, inverse_of: :project
   has_many :remixes, class_name: 'Project', foreign_key: 'remixed_from_id', dependent: :nullify, inverse_of: :parent
@@ -16,14 +17,20 @@ class Project < ApplicationRecord
 
   def check_unique_not_null
     self.identifier ||= PhraseIdentifier.generate
-    self.identifier = PhraseIdentifier.generate until has_allowed_identifier
+    self.identifier = PhraseIdentifier.generate until allowed_identifier?
   end
 
-  def has_allowed_identifier
-    if self.locale.nil?
+  def allowed_identifier?
+    if locale.nil?
       Project.find_by(identifier: self.identifier).nil?
     else
-      Project.find_by(identifier: self.identifier, locale:).nil?
+      Project.find_by(identifier: self.identifier, locale: self.locale).nil?
+    end
+  end
+
+  def identifier_cannot_be_taken_by_another_user
+    if (!Project.where(identifier: self.identifier).where.not(user_id: self.user_id).empty?)
+      errors.add(:identifier, "can't be taken by another user")
     end
   end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -14,6 +14,48 @@ RSpec.describe Project do
     end
   end
 
+  describe 'validations' do
+    let(:project) { create(:project) }
+    let(:identifier) { project.identifier }
+
+    it 'is invalid if no user or locale' do
+      invalid_project = build(:project, locale: nil, user_id: nil)
+      expect(invalid_project).to be_invalid
+    end
+
+    it 'is valid if user but no locale' do
+      valid_project = build(:project, locale: nil)
+      expect(valid_project).to be_valid
+    end
+
+    context 'same identifier and same user' do
+      let(:user_id) { project.user_id }
+
+      it 'is invalid if identifier in use by same user in the same locale' do
+        new_project = build(:project, identifier: identifier, user_id: user_id, locale: project.locale)
+        expect(new_project).to be_invalid
+      end
+
+      it 'is valid if identifier only in use by the user in the another locale' do
+        new_project = build(:project, identifier: identifier, user_id: user_id, locale: 'another_locale')
+        expect(new_project).to be_valid
+      end
+    end
+  
+    context 'same identifier but different user' do
+      let(:user_id) { 'another_user' }
+      it 'is invalid if identifier in use by another user in same locale' do
+        new_project = build(:project, identifier: identifier, user_id: user_id, locale: project.locale)
+        expect(new_project).to be_invalid
+      end
+
+      it 'is invalid if identifier in use in another locale by another user' do
+        new_project = build(:project, identifier: identifier, user_id: user_id, locale: 'another_locale')
+        expect(new_project).to be_invalid
+      end
+    end
+  end
+
   describe 'check_unique_not_null' do
     let(:saved_project) { create(:project) }
 
@@ -30,11 +72,6 @@ RSpec.describe Project do
     it 'does not change identifier if duplicated in different locale' do
       valid_project = build(:project, identifier: saved_project.identifier, locale: 'ja-JP')
       expect { valid_project.valid? }.not_to change(valid_project, :identifier)
-    end
-
-    it 'changes indentifier if duplicated with nil locale' do
-      user_project = build(:project, identifier: saved_project.identifier, locale: nil)
-      expect { user_project.valid? }.to change(user_project, :identifier)
     end
   end
 end
