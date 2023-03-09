75c15591526525dc50a1baf18c58e712a0ca3fe5
Lois Wells <lois.wells@raspberrypi.org>
2023-03-09 09:43:40 +0000

    Getting remix mutation working

--- app/graphql/mutations/remix_project.rb
+++ app/graphql/mutations/remix_project.rb
@@ -8,21 +8,20 @@ module Mutations
     field :project, Types::ProjectType, description: 'The project that has been created'
 
     def resolve(**input)
-      # project = GlobalID.find(input[:id])
       project = Project.find_by(identifier: input[:identifier], locale: input[:locale])
 
-      unless context[:current_ability]&.can?(:read, project)
-        raise GraphQL::ExecutionError, 'You are not permitted to read that project'
-      end
+      # unless context[:current_ability]&.can?(:read, project)
+      #   pp context[:current_ability]
+      #   pp context[:current_user_id]
+      #   raise GraphQL::ExecutionError, 'You are not permitted to read that project'
+      # end
 
       remix_params = {
-        project: {
-          name: project.name,
-          identifier: project.identifier,
-          components: project.components
-        }
+        name: project.name,
+        identifier: project.identifier,
+        components: project.components
       }
-      response = Project::Remix.call(remix_params: remix_params, user_id: context[:current_user_id], project: project)
+      response = Project::CreateRemix.call(params: remix_params, user_id: context[:current_user_id], original_project: project)
       raise GraphQL::ExecutionError, response[:error] unless response.success?
 
       { project: response[:project] }
