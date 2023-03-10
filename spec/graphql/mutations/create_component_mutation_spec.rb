eb214279012af363fce3554f58e4743d4cbd42a8
chrisruk <christopher.richardson@raspberrypi.org>
2023-03-10 15:38:58 +0000

    Create component spec

--- spec/graphql/mutations/create_component_mutation_spec.rb
+++ spec/graphql/mutations/create_component_mutation_spec.rb
@@ -5,8 +5,11 @@ require 'rails_helper'
 RSpec.describe 'mutation CreateComponent() { ... }' do
   subject(:result) { execute_query(query: mutation, variables:) }
 
+  let(:project) { create(:project) }
+  let(:project_id) { project.to_gid_param }
+
   let(:mutation) { 'mutation CreateComponent($component: CreateComponentInput!) { createComponent(input: $component) { component { id } } }' }
-  let(:project_id) { 'dummy-id' }
+  #let(:project_id) { 'dummy-id' }
   let(:variables) do
     {
       component: {
@@ -40,16 +43,38 @@ RSpec.describe 'mutation CreateComponent() { ... }' do
   end
 
   context 'when authenticated' do
-    userid = SecureRandom.uuid
-    let(:current_user_id) { userid }
-    let!(:project) { create(:project, user_id: userid) }
-    let(:project_id) { project.to_gid_param }
-
-    before { mock_phrase_generation }
+    let(:current_user_id) { SecureRandom.uuid }
+    let(:project) { create(:project, user_id: current_user_id) }
 
     it 'returns the component ID' do
       expect(result.dig('data', 'createComponent', 'component', 'id')).not_to be_nil 
     end
+
+    context 'when project id doesnt exist' do
+      let(:project_id) { 'dummy-id' }
+      it 'returns an error' do
+        expect(result.dig('errors', 0, 'message')).not_to be_blank
+      end
+    end
+
+    context 'when project id exists but belongs to someone else' do
+      let(:project) { create(:project) }
+      it 'returns an error' do
+        expect(result.dig('errors', 0, 'message')).not_to be_blank
+      end
+    end
+
+    context 'when project component fails to save' do
+      before do
+        component = Component.new
+        allow(component).to receive(:save).and_return(false)
+        allow(Component).to receive(:new).and_return(component)
+      end
+      it 'returns an error' do
+        expect(result.dig('errors', 0, 'message')).not_to be_nil
+      end
+    end
+
 
   end
 end
