dbe73d52e9814015c09825fbe81b351b109e843e
chrisruk <christopher.richardson@raspberrypi.org>
2023-03-13 10:25:50 +0000

    Fix rubocop issue

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,9 +2,11 @@
 
 module Types
   class MutationType < Types::BaseObject
+    # rubocop:disable GraphQL/ExtractType
     field :create_component, mutation: Mutations::CreateComponent, description: 'Create a component'
     field :create_project, mutation: Mutations::CreateProject, description: 'Create a project, complete with components'
     field :delete_project, mutation: Mutations::DeleteProject, description: 'Delete an existing project'
     field :update_project, mutation: Mutations::UpdateProject, description: 'Update fields on an existing project'
+    # rubocop:enable GraphQL/ExtractType
   end
 end
