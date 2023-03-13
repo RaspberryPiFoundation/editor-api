4f83019a1decf0ec242be13963a6ee74d1c61600
chrisruk <christopher.richardson@raspberrypi.org>
2023-03-13 14:31:01 +0000

    Initial component mutation spec

---
+++ spec/graphql/mutations/update_component_mutation_spec.rb
@@ -0,0 +1,90 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'mutation UpdateComponent() { ... }' do
+  subject(:result) { execute_query(query: mutation, variables:) }
+
+  let(:mutation) { 'mutation UpdateComponent($component: UpdateComponentInput!) { updateComponent(input: $component) { component { id } } }' }
+  let(:component_id) { 'dummy-id' }
+  let(:variables) do
+    {
+      component: {
+        id: component_id,
+        name: 'main2',
+        extension: 'py',
+        content: '',
+        default: false
+      }
+    }
+  end
+
+  it { expect(mutation).to be_a_valid_graphql_query }
+
+  context 'with an existing component' do
+    let(:project) { create(:project, user_id: SecureRandom.uuid, project_type: :python) }
+    let(:project_id) { project.to_gid_param }
+    let(:component) { create(:component, project:, name: 'bob', extension: 'py', content: '') }
+    let(:component_id) { component.to_gid_param }
+
+    before do
+      # Instantiate component
+      component
+    end
+
+    context 'when unauthenticated' do
+      it 'does not update a component' do
+        expect { result }.not_to change(component, :name)
+      end
+
+      it 'returns an error' do
+        expect(result.dig('errors', 0, 'message')).not_to be_blank
+      end
+    end
+
+    context 'when the graphql context is unset' do
+      let(:graphql_context) { nil }
+
+      it 'does not update a component' do
+        expect { result }.not_to change(component, :name)
+      end
+    end
+
+    context 'when authenticated' do
+      let(:current_user_id) { project.user_id }
+
+      it 'updates the component name' do
+        expect { result }.to change { component.reload.name }.from(component.name).to(variables.dig(:component, :name))
+      end
+
+      context 'when the component cannot be found' do
+        let(:component_id) { 'dummy' }
+
+        it 'returns an error' do
+          expect(result.dig('errors', 0, 'message')).to match(/not found/)
+        end
+      end
+
+      context 'with another users component' do
+        let(:current_user_id) { SecureRandom.uuid }
+
+        it 'returns an error' do
+          expect(result.dig('errors', 0, 'message')).to match(/not permitted/)
+        end
+      end
+
+      context 'when component update fails' do
+        before do
+          errors = instance_double(ActiveModel::Errors, full_messages: ['An error message'])
+          allow(component).to receive(:save).and_return(false)
+          allow(component).to receive(:errors).and_return(errors)
+          allow(GlobalID).to receive(:find).and_return(component)
+        end
+
+        it 'returns an error' do
+          expect(result.dig('errors', 0, 'message')).to match(/An error message/)
+        end
+      end
+    end
+  end
+end
