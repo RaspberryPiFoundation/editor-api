c6713f95970fd94810a8629cbc0ef8d906ee48b0
chrisruk <christopher.richardson@raspberrypi.org>
2023-03-13 11:48:42 +0000

    Fix rubocop issues

--- app/graphql/types/update_component_input_type.rb
+++ app/graphql/types/update_component_input_type.rb
@@ -4,11 +4,11 @@ module Types
   class UpdateComponentInputType < Types::BaseInputObject
     description 'Represents a project during an update'
 
-    argument :id, String, required: true, description: 'The ID of the component to update'
     argument :content, String, required: false, description: 'The text content of the component'
     argument :default, Boolean, required: true, description: 'If this is the default component on a project'
     argument :extension, String, required: true, description: 'The file extension of the component, e.g. html, csv, py'
+    argument :id, String, required: true, description: 'The ID of the component to update'
     argument :name, String, required: true, description: 'The name of the file'
-    #argument :project_id, String, required: true, description: 'The easy-to-rememeber identifier of the project'
+    # argument :project_id, String, required: true, description: 'The easy-to-rememeber identifier of the project'
   end
 end
