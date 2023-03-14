ca557ca1f534247a4e40b46b1cc8508fc836d1f4
chrisruk <christopher.richardson@raspberrypi.org>
2023-03-14 15:54:23 +0000

    Make ID required

--- app/graphql/types/update_component_input_type.rb
+++ app/graphql/types/update_component_input_type.rb
@@ -7,7 +7,7 @@ module Types
     argument :content, String, required: false, description: 'The text content of the component'
     argument :default, Boolean, required: false, description: 'If this is the default component on a project'
     argument :extension, String, required: false, description: 'The file extension of the component, e.g. html, csv, py'
-    argument :id, String, required: false, description: 'The ID of the component to update'
+    argument :id, String, required: true, description: 'The ID of the component to update'
     argument :name, String, required: false, description: 'The name of the file'
   end
 end
