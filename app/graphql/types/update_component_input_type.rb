3489001a8d41d2f6d205b1daddd671365c64e4d7
chrisruk <christopher.richardson@raspberrypi.org>
2023-03-14 11:21:08 +0000

    Make all fields optional

--- app/graphql/types/update_component_input_type.rb
+++ app/graphql/types/update_component_input_type.rb
@@ -5,9 +5,9 @@ module Types
     description 'Represents a project during an update'
 
     argument :content, String, required: false, description: 'The text content of the component'
-    argument :default, Boolean, required: true, description: 'If this is the default component on a project'
-    argument :extension, String, required: true, description: 'The file extension of the component, e.g. html, csv, py'
-    argument :id, String, required: true, description: 'The ID of the component to update'
-    argument :name, String, required: true, description: 'The name of the file'
+    argument :default, Boolean, required: false, description: 'If this is the default component on a project'
+    argument :extension, String, required: false, description: 'The file extension of the component, e.g. html, csv, py'
+    argument :id, String, required: false, description: 'The ID of the component to update'
+    argument :name, String, required: false, description: 'The name of the file'
   end
 end
