aff1d2a3fcf01e3b382b69bbb0bff9fe43f2f7b0
Pete Bell <104009652+pjbRPF@users.noreply.github.com>
2023-06-27 18:09:58 +0100

    Changes for editor (#180)

--- lib/tasks/project_components/hello_world_example/main.py
+++ lib/tasks/project_components/hello_world_example/main.py
@@ -13,10 +13,10 @@ fire = '🔥'
 # Useful characters :',()*_/.#
 
 # Function definitions  
-def roll_dice(): 
-    max = input('How many sides for a 🎲?: ') # Wait for input from the user    
-    print('Rolling a', max, 'sided dice ...') # Use the number the user entered  
-    roll = randint(1, int(max)) # Generate a random number between 1 and 6    
+def roll_dice():   
+    max = input('How many sides?:') # Wait for input from the user    
+    print('That\'s a D', max) # Use the number the user entered    
+    roll = randint(1, int(max)) # Use max to determine the number of sides the dice has
     print('You rolled a', roll, fire * roll) # Repeat the fire emoji to match the dice roll
   
 # Put code to run under here
@@ -24,7 +24,7 @@ print('Hello', world)
 print('Welcome to', python) 
 print(python, 'is very good at maths!')   
 print(230 * 5782 ** 2 / 23781) # Print the result of the sum  
-print('The 📅 🕒 is', datetime.now()) # Print with emojis 
+print('The date and time is', datetime.now()) # Print the current date and time
 
 roll_dice() # Call the roll dice function
 print('I ❤️ rainbows 🌈')   
