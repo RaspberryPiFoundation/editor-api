d7f9b231fb44549cabbe007c478a49644ecebfe7
Scott <scott.adams@raspberrypi.org>
2023-06-29 09:13:27 +0100

    fix(error_explanation_resolver): un-pirate the LLM

--- app/graphql/resolvers/error_explanation_resolver.rb
+++ app/graphql/resolvers/error_explanation_resolver.rb
@@ -11,11 +11,16 @@ module Resolvers
         parameters: {
           model: "gpt-3.5-turbo",
           messages: [
-            {role: "system", content: "You are a kind pirate talking to a five year old child."},
+            {role: "system", content: "You are a kind teacher talking to a five year old child."},
+            # {role: "system", content: "You are a kind pirate talking to a five year old child."}, // Initial pirate version
+            # {role: "system", content: "Explain to a 8 year old using short simple sentences the problem and provide a solution or example."},
+            # {role: "system", content: "Without using the code provided explain the problem in simple sentences and provide an example."},
             {role: "user", content: "Explain the error \"#{error}\" in the following python code: \"#{code}\""}
           ],
           temperature: 0.5
         })
+
+      puts response.inspect
 
       {message: response.dig("choices", 0, "message", "content")}
     end
