5ed45e3933c6762fa3eaeab28f7177c55cd413c1
Pete Bell <104009652+pjbRPF@users.noreply.github.com>
2023-07-13 15:01:57 +0100

    Update main.py Comments PEP8

--- lib/tasks/project_components/mapping_data_carbon_data/main.py
+++ lib/tasks/project_components/mapping_data_carbon_data/main.py
@@ -20,32 +20,32 @@ def draw_pin(x, y, colour):
   triangle(x-10, y+5, x, y-10, x+10, y+5)
 
 def draw_data():
-  red_value = 255 # Set a starting value for red
+  red_value = 255  # Set a starting value for red
   blue_value = 0
   green_value = 255
   for region in region_list:
-    region_name = region['region'] # Get the name of the region
-    region_coords = get_region_coords(region_name) # Use the name to get coordinates
-    region_x = region_coords['x'] # Get the x coordinate
-    region_y = region_coords['y'] # Get the y coordinate
+    region_name = region['region']  # Get the name of the region
+    region_coords = get_region_coords(region_name)  # Use the name to get coordinates
+    region_x = region_coords['x']  # Get the x coordinate
+    region_y = region_coords['y']  # Get the y coordinate
     #print(region_name, region_x, region_y)
-    region_colour = Color(red_value, green_value, blue_value) # Use the red value in the colour
+    region_colour = Color(red_value, green_value, blue_value)  # Use the red value in the colour
     colours[region_colour.hex] = region
-    draw_pin(region_x, region_y, region_colour) # Draw the pin
-    red_value -= 1 # Change the red value
-    green_value += 1 #Change the green value
-    blue_value -= 1 #Change the blue value
+    draw_pin(region_x, region_y, region_colour)  # Draw the pin
+    red_value -= 1  # Change the red value
+    green_value += 1  #Change the green value
+    blue_value -= 1  #Change the blue value
   
 # Put code to run every frame here
 def draw():
   image(
-    map, # The image to draw
-    0, # The x of the top-left corner
-    0, # The y of the top-left corner
-    width, # The width of the image
-    height # The height of the image
+    map,  # The image to draw
+    0,  # The x of the top-left corner
+    0,  # The y of the top-left corner
+    width,  # The width of the image
+    height  # The height of the image
     )
-  #draw_pin(200, 200, color(255,0,0))
+  #draw_pin(200, 200, Color(255,0,0))
   draw_data()
 
 
