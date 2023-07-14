eb71631aba2d33ca1bb7035845f9be45ed35eb77
Mac Bowley <36036152+m-bowley@users.noreply.github.com>
2023-07-14 09:51:46 +0100

    Update main.py - fixing tabs

--- lib/tasks/project_components/persuasive_data_presentation_dogs_example/main.py
+++ lib/tasks/project_components/persuasive_data_presentation_dogs_example/main.py
@@ -5,52 +5,52 @@
 from pygal import *
 
 with open('dog_breed_characteristics.csv') as f:
-  data = f.read()
-  lines = data.splitlines()
+    data = f.read()
+    lines = data.splitlines()
 
 choice = 0
 
 def main():
   
-  choice = input('Analysing Toy dogs. What would you like to see? \n1. Average price, \n2. Popularity, \n3. Intelligence, \nChoice:')
-  
-  if choice == '1':
-    chart = Bar(width=600, height=400, title=' 🐶 Average price of Toy dogs by breed 🐶 ')
-    for line in lines:
-      info = line.split(',')
-      BreedName = info[0]
-      Type = info[1]
-      AvgPrice = info[2]
-      if Type == 'Toy':
-        chart.add(BreedName, float(AvgPrice))
-    chart.render()
+    choice = input('Analysing Toy dogs. What would you like to see? \n1. Average price, \n2. Popularity, \n3. Intelligence, \nChoice:')
+    
+    if choice == '1':
+        chart = Bar(width=600, height=400, title=' 🐶 Average price of Toy dogs by breed 🐶 ')
+        for line in lines:
+            info = line.split(',')
+            BreedName = info[0]
+            Type = info[1]
+            AvgPrice = info[2]
+            if Type == 'Toy':
+                chart.add(BreedName, float(AvgPrice))
+        chart.render()
     main()
       
-  if choice == '2':
-    chart = Pie(width=600, height=400, title='🐶 Popularity of Toy dogs by breed 🐶 ')
-    for line in lines:
-      info = line.split(',')
-      BreedName = info[0]
-      Type = info[1]
-      AvgPrice = info[2]
-      Intelligence = info[3]
-      Popularity = info[4]
-      if Type == 'Toy':
-        chart.add(BreedName, float(Popularity))
-    chart.render()
-    main()
+    if choice == '2':
+        chart = Pie(width=600, height=400, title='🐶 Popularity of Toy dogs by breed 🐶 ')
+        for line in lines:
+            info = line.split(',')
+            BreedName = info[0]
+            Type = info[1]
+            AvgPrice = info[2]
+            Intelligence = info[3]
+            Popularity = info[4]
+            if Type == 'Toy':
+                chart.add(BreedName, float(Popularity))
+        chart.render()
+        main()
     
-  if choice == '3':
+    if choice == '3':
     chart = Bar(width=600, height=400, title='🐶 Intelligence of Toy dogs by breed 🐶')
     for line in lines:
-      info = line.split(',')
-      BreedName = info[0]
-      Type = info[1]
-      AvgPrice = info[2]
-      Intelligence = info[3]
-      Popularity = info[4]
-      if Type == 'Toy':
-        chart.add(BreedName, float(Intelligence))
+        info = line.split(',')
+        BreedName = info[0]
+        Type = info[1]
+        AvgPrice = info[2]
+        Intelligence = info[3]
+        Popularity = info[4]
+        if Type == 'Toy':
+            chart.add(BreedName, float(Intelligence))
     chart.render()
     main()
 
