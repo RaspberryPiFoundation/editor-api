00531d7fda9a0c3080dd2f8874dbe3d8da4edcc2
Mac Bowley <36036152+m-bowley@users.noreply.github.com>
2023-07-14 14:43:02 +0100

    Update main.py - fixing tabs

--- lib/tasks/project_components/charting_champions_example/main.py
+++ lib/tasks/project_components/charting_champions_example/main.py
@@ -6,17 +6,17 @@ chart = Bar(title='Olympic medals')
 
 # Add data to the chart
 with open('medals.csv') as f:
-  data = f.read()
-  lines = data.splitlines()
-  #print(lines)
+    data = f.read()
+    lines = data.splitlines()
+    #print(lines)
 
 for line in lines:
-  tally = line.split(',')
-  #print(tally)
-
-  team = tally[0]
-  medals = tally[1]
-  chart.add(team, int(medals))  # Make medals a number
+    tally = line.split(',')
+    #print(tally)
+    
+    team = tally[0]
+    medals = tally[1]
+    chart.add(team, int(medals))  # Make medals a number
 
 # Display the chart
 chart.render()
