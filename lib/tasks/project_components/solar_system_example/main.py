d18a03f9cd0d83ea8cacb0bfe52aa1864e04f9ef
Pete Bell <104009652+pjbRPF@users.noreply.github.com>
2023-07-21 11:44:48 +0100

    Update main.py Comments to PEP8

--- lib/tasks/project_components/solar_system_example/main.py
+++ lib/tasks/project_components/solar_system_example/main.py
@@ -3,14 +3,14 @@ from p5 import *
 from make_planet import make_planet
 
 def draw_sun():
-    fill(255, 255, 0) # Yellow
+    fill(255, 255, 0)  # Yellow
     ellipse(width / 2 , height / 2, 100, 100)
 
 
 # draw_orbits function
 def draw_orbits():
     no_fill()
-    stroke(255) # Make it white
+    stroke(255)  # Make it white
 
     ellipse(width / 2, height / 2, mercury['orbit'], mercury['orbit'])
     ellipse(width / 2, height / 2, venus['orbit'], venus['orbit'])
@@ -73,13 +73,13 @@ def load_planets():
         data = f.read()
         lines = data.splitlines()
   
-    planet = lines[2].split(',') # Split Venus' data
+    planet = lines[2].split(',')  # Split Venus' data
     venus = { 
         'name': planet[0],
         'colour': Color(int(planet[1]), int(planet[2]), int(planet[3])),
-        'size': int(planet[4]), # int() for whole numbers
+        'size': int(planet[4]),  # int() for whole numbers
         'orbit': int(planet[5]),
-        'speed': float(planet[6]), # float() for decimals
+        'speed': float(planet[6]),  # float() for decimals
         'info': planet[7]
     }
 
