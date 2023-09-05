1bc140c0aa5380bb0be8b315379ba51fe3d2b996
Lois Wells <lois.wells@raspberrypi.org>
2023-09-05 17:22:24 +0100

    remove migration that wasn't doing anything

--- db/migrate/20230904160441_nullify_project_error_reference_on_delete.rb
+++
@@ -1,9 +0,0 @@
-class NullifyProjectErrorReferenceOnDelete < ActiveRecord::Migration[7.0]
-  def up
-    change_column :project_errors, :project_id, :uuid, foreign_key: true, on_delete: :nullify
-  end
-
-  def down
-    change_column :project_errors, :project_id, :uuid, foreign_key: true, on_delete: :restrict
-  end
-end
