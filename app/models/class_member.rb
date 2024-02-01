1c75c70fcd0ce92d5c0c7d718f1f7e08fd400a3e
Chris Patuzzo <chris@patuzzo.co.uk>
2024-02-01 17:22:00 +0000

    Add a ClassMember model

---
+++ app/models/class_member.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class ClassMember < ApplicationRecord
+  belongs_to :school_class
+
+  validates :student_id, presence: true, uniqueness: {
+    scope: :school_class_id,
+    case_sensitive: false
+  }
+end
---
+++ spec/models/class_member_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ClassMember do
+  describe 'validations' do
+    subject(:class_member) { build(:class_member) }
+
+    it 'has a valid default factory' do
+      expect(class_member).to be_valid
+    end
+
+    it 'can save the default factory' do
+      expect { class_member.save! }.not_to raise_error
+    end
+
+    it 'requires a school_class' do
+      class_member.school_class = nil
+      expect(class_member).to be_invalid
+    end
+
+    it 'requires a student_id' do
+      class_member.student_id = ' '
+      expect(class_member).to be_invalid
+    end
+
+    it 'requires a UUID student_id' do
+      class_member.student_id = 'invalid'
+      expect(class_member).to be_invalid
+    end
+
+    it 'requires a unique student_id within the school_class' do
+      class_member.save!
+      duplicate = build(:class_member, student_id: class_member.student_id, school_class: class_member.school_class)
+      expect(duplicate).to be_invalid
+    end
+  end
+end
