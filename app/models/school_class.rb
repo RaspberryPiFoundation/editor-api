aade1a7d76d33e13e1b89cb5655c8ac803d73b42
Chris Patuzzo <chris@patuzzo.co.uk>
2024-02-18 14:31:36 +0000

    Add a SchoolClass#lessons association

--- app/models/school_class.rb
+++ app/models/school_class.rb
@@ -3,6 +3,7 @@
 class SchoolClass < ApplicationRecord
   belongs_to :school
   has_many :members, class_name: :ClassMember, inverse_of: :school_class, dependent: :destroy
+  has_many :lessons, dependent: :nullify
 
   validates :teacher_id, presence: true
   validates :name, presence: true
