0d2280a96bab49d6de8cf115b5121c1bb868128f
Chris Patuzzo <chris@patuzzo.co.uk>
2024-02-21 18:39:32 +0000

    Add a lessons.copied_from column
    The naming follows the ‘remixed_from’ convention on projects.

--- db/schema.rb
+++ db/schema.rb
@@ -127,6 +127,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_17_144009) do
   create_table "lessons", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.uuid "school_id"
     t.uuid "school_class_id"
+    t.uuid "copied_from"
     t.uuid "user_id", null: false
     t.string "name", null: false
     t.string "description"
@@ -136,6 +137,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_17_144009) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["archived_at"], name: "index_lessons_on_archived_at"
+    t.index ["copied_from"], name: "index_lessons_on_copied_from"
     t.index ["name"], name: "index_lessons_on_name"
     t.index ["school_class_id"], name: "index_lessons_on_school_class_id"
     t.index ["school_id"], name: "index_lessons_on_school_id"
@@ -202,6 +204,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_17_144009) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "class_members", "school_classes"
   add_foreign_key "components", "projects"
+  add_foreign_key "lessons", "lessons", column: "copied_from"
   add_foreign_key "lessons", "school_classes"
   add_foreign_key "lessons", "schools"
   add_foreign_key "project_errors", "projects"
