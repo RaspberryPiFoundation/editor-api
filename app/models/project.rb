298a5061a20fb468bd882bd65267a42cb43e4c52
Chris Patuzzo <chris@patuzzo.co.uk>
2024-02-23 15:14:01 +0000

    Add Project.users, .with_users and #with_user methods

--- app/models/project.rb
+++ app/models/project.rb
@@ -17,6 +17,19 @@ class Project < ApplicationRecord
   validate :identifier_cannot_be_taken_by_another_user
   validates :locale, presence: true, unless: :user_id
 
+  def self.users
+    User.from_userinfo(ids: pluck(:user_id))
+  end
+
+  def self.with_users
+    by_id = users.index_by(&:id)
+    all.map { |instance| [instance, by_id[instance.user_id]] }
+  end
+
+  def with_user
+    [self, User.from_userinfo(ids: user_id).first]
+  end
+
   # Work around a CanCanCan issue with accepts_nested_attributes_for.
   # https://github.com/CanCanCommunity/cancancan/issues/774
   def components=(array)
