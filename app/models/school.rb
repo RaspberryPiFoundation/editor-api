3f1b8e80b6ae43821d5048f12df4d502885f608e
Dan Halson <dan.halson@raspberrypi.org>
2024-04-19 15:59:33 +0100

    Tweak validations

--- app/models/school.rb
+++ app/models/school.rb
@@ -5,12 +5,12 @@ class School < ApplicationRecord
   has_many :lessons, dependent: :nullify
   has_many :projects, dependent: :nullify
 
-  validates :id, presence: true, uniqueness: { case_sensitive: false }
   validates :name, presence: true
-  validates :reference, uniqueness: { case_sensitive: false, allow_nil: true }
+  validates :website, presence: true
   validates :address_line_1, presence: true
   validates :municipality, presence: true
   validates :country_code, presence: true, inclusion: { in: ISO3166::Country.codes }
+  validates :urn, uniqueness: { case_sensitive: false, allow_nil: true }
 
   def valid_except_for_id?
     validate
