981d7541854d64922cf56599fe9aa09002582b5b
Dan Halson <dan.halson@raspberrypi.org>
2024-04-29 17:40:18 +0100

    Revert stubbed organisation_id removal

--- lib/concepts/school/operations/create.rb
+++ lib/concepts/school/operations/create.rb
@@ -3,9 +3,9 @@
 class School
   class Create
     class << self
-      def call(school_params:, user_id:)
+      def call(school_params:, user_id:, token:)
         response = OperationResponse.new
-        response[:school] = build_school(school_params.merge!(user_id:))
+        response[:school] = build_school(school_params.merge!(user_id:), token:)
         response[:school].save!
         response
       rescue StandardError => e
@@ -16,8 +16,15 @@ class School
 
       private
 
-      def build_school(school_params)
-        School.new(school_params)
+      def build_school(school_params, token:)
+        school = School.new(school_params)
+
+        if school.valid_except_for_id?
+          response = ProfileApiClient.create_organisation(token:)
+          school.id = response&.fetch(:id)
+        end
+
+        school
       end
     end
   end
