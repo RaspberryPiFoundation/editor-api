05e2d1c4570f37d7f3e34383fe2c98bce7f471ce
Chris Roos <chris@seagul.co.uk>
2024-05-20 11:18:13 +0100

    Extract `User::STUDENT_WITHOUT_ORGANISATIONS_ID`
    To remove some duplication.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -85,7 +85,7 @@ RSpec.describe User do
     end
 
     context 'when no organisations are returned' do
-      let(:ids) { ['33333333-3333-3333-3333-333333333333'] } # student without organisations
+      let(:ids) { [User::STUDENT_WITHOUT_ORGANISATIONS_ID] }
 
       it 'returns a user with the correct organisations' do
         stub_user_info_api_for_student_without_organisations
