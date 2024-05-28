4a41400efadf73a79a4c2395a0aef535bb4b697e
Chris Roos <chris@seagul.co.uk>
2024-05-28 13:39:36 +0100

    Add /school resource
    To allow the logged in user to request information about their school
    and their roles within it.
    
    The JSON response mirrors that of the /api/schools/:id resource with the
    addition of the user's roles.

---
+++ spec/features/my_school/showing_my_school_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Showing my school', type: :request do
+  before do
+    authenticate_as_school_owner(school_id: school.id, owner_id:)
+  end
+
+  let!(:school) { create(:school, name: 'school-name') }
+  let(:headers) { { Authorization: UserProfileMock::TOKEN } }
+  let(:owner_id) { SecureRandom.uuid }
+
+  it 'responds 200 OK' do
+    get('/api/school', headers:)
+    expect(response).to have_http_status(:ok)
+  end
+
+  it "includes the school details and user's roles in the JSON" do
+    school_json = school.to_json(only: %i[id name website reference address_line_1 address_line_2 municipality administrative_area postal_code country_code verified_at created_at updated_at])
+    expected_data = JSON.parse(school_json, symbolize_names: true).merge(roles: ['owner'])
+
+    get('/api/school', headers:)
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(data).to eq(expected_data)
+  end
+
+  it "responds 404 Not Found when the user doesn't have a role in any school" do
+    Role.find_by(school:, user_id: owner_id).delete
+    get('/api/school', headers:)
+    expect(response).to have_http_status(:not_found)
+  end
+
+  it 'responds 401 Unauthorized when no token is given' do
+    get '/api/school'
+    expect(response).to have_http_status(:unauthorized)
+  end
+end
