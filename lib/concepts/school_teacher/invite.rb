81f4833d28264d55e6b76e1fc2ce4afd7565e535
Chris Lowis <chris.lowis@gofreerange.com>
2024-06-05 11:51:58 +0100

    SchoolTeacher#invite does not need to call ProfileApiClient
    We are going to handle the invitation of school teachers in this
    application rather than in the profile application, so this method no
    longer needs to call ProfileApiClient#invite_school_teacher. Note the
    latter method is currently a no-op, so this doesn't change any
    existing behaviour.

--- lib/concepts/school_teacher/invite.rb
+++ lib/concepts/school_teacher/invite.rb
@@ -15,13 +15,11 @@ module SchoolTeacher
 
       private
 
-      def invite_teacher(school, school_teacher_params, token)
+      def invite_teacher(school, school_teacher_params, _token)
         email_address = school_teacher_params.fetch(:email_address)
 
         raise ArgumentError, 'school is not verified' unless school.verified_at
         raise ArgumentError, "email address '#{email_address}' is invalid" unless EmailValidator.valid?(email_address)
-
-        ProfileApiClient.invite_school_teacher(token:, email_address:, organisation_id: school.id)
       end
     end
   end
--- spec/concepts/school_teacher/invite_spec.rb
+++ spec/concepts/school_teacher/invite_spec.rb
@@ -11,21 +11,9 @@ RSpec.describe SchoolTeacher::Invite, type: :unit do
     { email_address: 'teacher-to-invite@example.com' }
   end
 
-  before do
-    stub_profile_api_invite_school_teacher
-  end
-
   it 'returns a successful operation response' do
     response = described_class.call(school:, school_teacher_params:, token:)
     expect(response.success?).to be(true)
-  end
-
-  it 'makes a profile API call' do
-    described_class.call(school:, school_teacher_params:, token:)
-
-    # TODO: Replace with WebMock assertion once the profile API has been built.
-    expect(ProfileApiClient).to have_received(:invite_school_teacher)
-      .with(token:, email_address: 'teacher-to-invite@example.com', organisation_id: school.id)
   end
 
   context 'when creation fails' do
@@ -35,11 +23,6 @@ RSpec.describe SchoolTeacher::Invite, type: :unit do
 
     before do
       allow(Sentry).to receive(:capture_exception)
-    end
-
-    it 'does not make a profile API request' do
-      described_class.call(school:, school_teacher_params:, token:)
-      expect(ProfileApiClient).not_to have_received(:invite_school_teacher)
     end
 
     it 'returns a failed operation response' do
