7da7d19c849155bfb851e71ad04e1a025614f80e
Chris Lowis <chris.lowis@gofreerange.com>
2024-06-05 16:45:47 +0100

    Revert "Do not load ActionMailer"
    This reverts commit b80d7744916b73eb1d38e159789f24d50f59b7ec. We now
    have the requirement to send an invitation email to a teacher so it
    makes sense to bring back the code that was removed in this commit.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,12 @@ Rails.application.configure do
   # config.active_job.queue_adapter = :resque
   # config.active_job.queue_name_prefix = "app_production"
 
+  config.action_mailer.perform_caching = false
+
+  # Ignore bad email addresses and do not raise email delivery errors.
+  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  # config.action_mailer.raise_delivery_errors = false
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
