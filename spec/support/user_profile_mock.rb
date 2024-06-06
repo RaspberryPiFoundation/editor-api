4c6c0653fe987884b1b1373dec9b9624a91c8af1
Chris Roos <chris@seagul.co.uk>
2024-06-06 15:57:32 +0100

    Pass user attributes to `#stub_hydra_public_api`
    In preparation for being able to use the attributes from the `User`
    instances passed to `authenticate_as_xxx` instead of those from the
    users.json fixture file.

--- spec/support/user_profile_mock.rb
+++ spec/support/user_profile_mock.rb
@@ -13,19 +13,28 @@ module UserProfileMock
   end
 
   def authenticate_as_school_owner(owner)
-    stub_hydra_public_api(user_index: 0, user_id: owner.id)
+    user = user_attributes_by_index(0)
+    user['id'] = owner.id
+
+    stub_hydra_public_api(user)
   end
 
   def authenticate_as_school_teacher(teacher)
-    stub_hydra_public_api(user_index: 1, user_id: teacher.id)
+    user = user_attributes_by_index(1)
+    user['id'] = teacher.id
+
+    stub_hydra_public_api(user)
   end
 
   def authenticate_as_school_student(student)
-    stub_hydra_public_api(user_index: 2, user_id: student.id)
+    user = user_attributes_by_index(2)
+    user['id'] = student.id
+
+    stub_hydra_public_api(user)
   end
 
   def unauthenticated_user
-    stub_hydra_public_api(user_index: nil)
+    stub_hydra_public_api({})
   end
 
   def stubbed_user
@@ -47,16 +56,13 @@ module UserProfileMock
   end
 
   # Stubs the API that returns user profile data for the logged in user.
-  def stub_hydra_public_api(user_index:, user_id: nil)
-    user_attrs = user_attributes_by_index(user_index)
-    user_attrs['id'] = user_id if user_id
-
+  def stub_hydra_public_api(user)
     stub_request(:get, "#{HydraPublicApiClient::API_URL}/userinfo")
       .with(headers: { Authorization: "Bearer #{TOKEN}" })
       .to_return(
         status: 200,
         headers: { content_type: 'application/json' },
-        body: user_attrs.to_json
+        body: user.to_json
       )
   end
 
