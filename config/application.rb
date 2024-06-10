87ea1356e2e60d9a0f9d9c55892586dfa2fc7041
Chris Lowis <chris.lowis@gofreerange.com>
2024-06-10 13:27:46 +0100

    run rails app:update
    As per the rails upgrade guide[1] I ran
    
        rails app:update
    
    and then
    
        rubocop -A
    
    to bring the code generated by rails app:update into line with our
    style guide.
    
    I discarded any changes that removed code we'd explicitly added to,
    for example, config/application.rb.
    
    [1] https://guides.rubyonrails.org/upgrading_ruby_on_rails.html
    
    We hadn't set the `config.force_ssl` option to true back when the app
    was created in 2b09a14aa. It feels like the kind of thing that might
    cause an issue, so I've decided to leave it unset with this upgrade too.

--- config/application.rb
+++ config/application.rb
@@ -9,11 +9,11 @@ require 'active_job/railtie'
 require 'active_record/railtie'
 require 'active_storage/engine'
 require 'action_controller/railtie'
-# require 'action_mailer/railtie'
-# require 'action_mailbox/engine'
-# require 'action_text/engine'
+# require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
 require 'action_view/railtie'
-# require 'action_cable/engine'
+# require "action_cable/engine"
 # require "rails/test_unit/railtie"
 require_relative '../lib/corp_middleware'
 
@@ -29,6 +29,7 @@ module App
     config.autoload_paths << "#{root}/lib"
     config.autoload_paths << "#{root}/lib/concepts"
     Rails.autoloaders.main.collapse('lib/concepts/*/operations')
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -37,7 +38,6 @@ module App
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    # config.autoload_paths += %W["#{config.root}/lib"]
 
     config.generators do |g|
       g.orm :active_record, primary_key_type: :uuid
@@ -55,5 +55,6 @@ module App
     config.api_only = false
 
     config.middleware.insert_before 0, CORPMiddleware
+    config.generators.system_tests = nil
   end
 end
