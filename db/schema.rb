5a8fe6f05f166e8f48f72ebdab31f003c6277ef7
James Mead <james.mead+rpf@gofreerange.com>
2024-06-21 10:55:33 +0100

    Add Invitation#accepted_at timestamp
    I think it's going to be useful to keep track of when a given invitation
    was accepted.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_12_153270) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_21_082741) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -156,6 +156,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_12_153270) do
     t.uuid "school_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "accepted_at"
     t.index ["school_id"], name: "index_invitations_on_school_id"
   end
 
