ace50a208582f136f0dab782dfb1d934b9ee8915
Dan Halson <dan.halson@raspberrypi.org>
2024-08-08 09:37:46 +0100

    394: Additional testing around when a project is auto created on the back of a lesson

--- spec/concepts/lesson/create_spec.rb
+++ spec/concepts/lesson/create_spec.rb
@@ -3,12 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe Lesson::Create, type: :unit do
-  let(:teacher_id) { SecureRandom.uuid }
+  let(:school) { create(:school) }
+  let(:student) { create(:student, school:) }
+  let(:teacher) { create(:teacher, school:) }
 
   let(:lesson_params) do
     {
       name: 'Test Lesson',
-      user_id: teacher_id,
+      user_id: teacher.id,
+      school_id: school.id,
       project_attributes: {
         name: 'Hello world project',
         project_type: 'python',
@@ -19,37 +22,63 @@ RSpec.describe Lesson::Create, type: :unit do
     }
   end
 
-  it 'returns a successful operation response' do
-    response = described_class.call(lesson_params:)
-    expect(response.success?).to be(true)
-  end
+  context 'when a teacher' do
+    before do
+      allow(User).to receive(:from_userinfo).with(ids: teacher.id).and_return([teacher])
+    end
 
-  it 'creates a lesson' do
-    expect { described_class.call(lesson_params:) }.to change(Lesson, :count).by(1)
-  end
+    it 'returns a successful operation response' do
+      response = described_class.call(lesson_params:)
+      expect(response.success?).to be(true)
+    end
 
-  it 'returns the lesson in the operation response' do
-    response = described_class.call(lesson_params:)
-    expect(response[:lesson]).to be_a(Lesson)
-  end
+    it 'creates a lesson' do
+      expect { described_class.call(lesson_params:) }.to change(Lesson, :count).by(1)
+    end
 
-  it 'assigns the name' do
-    response = described_class.call(lesson_params:)
-    expect(response[:lesson].name).to eq('Test Lesson')
-  end
+    it 'returns the lesson in the operation response' do
+      response = described_class.call(lesson_params:)
+      expect(response[:lesson]).to be_a(Lesson)
+    end
 
-  it 'creates a project for the lesson' do
-    expect { described_class.call(lesson_params:) }.to change(Project, :count).by(1)
-  end
+    it 'assigns the name' do
+      response = described_class.call(lesson_params:)
+      expect(response[:lesson].name).to eq('Test Lesson')
+    end
 
-  it 'associates the project to the lesson' do
-    response = described_class.call(lesson_params:)
-    expect(response[:lesson].project).to be_a(Project)
-  end
+    it 'assigns the user_id' do
+      response = described_class.call(lesson_params:)
+      expect(response[:lesson].user_id).to eq(teacher.id)
+    end
 
-  it 'assigns the user_id' do
-    response = described_class.call(lesson_params:)
-    expect(response[:lesson].user_id).to eq(teacher_id)
+    it 'assigns the school_id' do
+      response = described_class.call(lesson_params:)
+      expect(response[:lesson].school_id).to eq(school.id)
+    end
+
+    it 'creates a project for the lesson' do
+      expect { described_class.call(lesson_params:) }.to change(Project, :count).by(1)
+    end
+
+    it 'associates the project to the lesson' do
+      response = described_class.call(lesson_params:)
+      expect(response[:lesson].project).to be_a(Project)
+    end
+
+    it 'assigns the user id to the project' do
+      response = described_class.call(lesson_params:)
+      expect(response[:lesson].project.user_id).to eq(response[:lesson].user_id)
+    end
+
+    it 'assigns the school id to the project' do
+      response = described_class.call(lesson_params:)
+      expect(response[:lesson].project.school_id).to eq(response[:lesson].school_id)
+    end
+
+    it 'assigns the lesson id to the project' do
+      response = described_class.call(lesson_params:)
+      expect(response[:lesson].project.lesson_id).to eq(response[:lesson].id)
+    end
   end
 
   context 'when lesson creation fails' do
@@ -97,7 +126,6 @@ RSpec.describe Lesson::Create, type: :unit do
     let(:lesson_params) do
       {
         name: 'Test Lesson',
-        user_id: teacher_id,
         project_attributes: {
           invalid_attribute: 'blah blah blah'
         }
