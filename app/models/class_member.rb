a276520fb9d2299218eb2163d94bcad6e862fc68
Conor <conor.riches@raspberrypi.org>
2024-09-12 12:06:26 +0100

    remove conditional log

--- app/models/class_member.rb
+++ app/models/class_member.rb
@@ -13,7 +13,6 @@ class ClassMember < ApplicationRecord
   validate :student_has_the_school_student_role_for_the_school
 
   has_paper_trail(
-    if: ->(cm) { cm.school_class&.school_id },
     meta: {
       meta_school_id: ->(cm) { cm.school_class&.school_id }
     }
