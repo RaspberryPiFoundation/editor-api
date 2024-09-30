1a3f179919228fb1e907308fea163c9edacd2208
Scott <scott.adams@raspberrypi.org>
2024-09-30 17:08:12 +0100

    add includes for images_attachments only for show action

--- config/initializers/bullet.rb
+++ config/initializers/bullet.rb
@@ -1,3 +1,1 @@
-if Rails.env.development? || Rails.env.test?
-  Bullet.add_safelist type: :unused_eager_loading, class_name: 'Project', association: :images_attachments
-end
+Bullet.add_safelist type: :unused_eager_loading, class_name: 'Project', association: :images_attachments if Rails.env.development? || Rails.env.test?
