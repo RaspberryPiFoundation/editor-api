1022f3e0170bba0cdae3c397e35583a09a0a96cc
Dan Halson <dan.halson@raspberrypi.org>
2024-11-11 09:49:09 +0000

    Don't try and encrypt password if none passed

--- lib/concepts/school_student/update.rb
+++ lib/concepts/school_student/update.rb
@@ -17,9 +17,9 @@ module SchoolStudent
 
       def update_student(school, student_id, school_student_params, token)
         username = school_student_params.fetch(:username, nil)
-        encrypted_password = school_student_params.fetch(:password, nil)
-        password = DecryptionHelpers.decrypt_password(encrypted_password)
         name = school_student_params.fetch(:name, nil)
+        password = school_student_params.fetch(:password, nil)
+        password = DecryptionHelpers.decrypt_password(password) if password.present?
 
         validate(username:, password:, name:)
 
