cc3ad07e6d3eac40f1ba980b43e74e254456c18d
KristinaDudnyk <kristina.dudnyk20@gmail.com>
2024-11-21 15:46:02 +0000

    Deleted mistake

--- db/migrate/20240523143911_add_consent_fields.rb
+++ db/migrate/20240523143911_add_consent_fields.rb
@@ -1,6 +1,6 @@
 class AddConsentFields < ActiveRecord::Migration[7.0]
   def change
     add_column :schools, :creator_agree_authority, :boolean
-    add_column :schools, :creator_agree_terms_and_conditions, :boolean, default: false
+    add_column :schools, :creator_agree_terms_and_conditions, :boolean
   end
 end
