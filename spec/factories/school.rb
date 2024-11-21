ab06730d224de0473cfc0222f6ba9b5280988d1b
KristinaDudnyk <kristina.dudnyk20@gmail.com>
2024-11-21 16:16:42 +0000

    Removed attribute in the factory

--- spec/factories/school.rb
+++ spec/factories/school.rb
@@ -10,7 +10,6 @@ FactoryBot.define do
     creator_id { SecureRandom.uuid }
     creator_agree_authority { true }
     creator_agree_terms_and_conditions { true }
-    creator_agree_to_ux_contact { false }
   end
 
   factory :verified_school, parent: :school do
