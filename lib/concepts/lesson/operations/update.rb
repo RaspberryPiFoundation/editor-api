d8e49d701cfaa5248198446fd8a28a56258dbc06
Lois Wells <lois.wells@raspberrypi.org>
2024-12-02 14:36:22 +0000

    renaming lesson project and student remixes

--- lib/concepts/lesson/operations/update.rb
+++ lib/concepts/lesson/operations/update.rb
@@ -8,12 +8,27 @@ class Lesson
         response[:lesson] = lesson
         response[:lesson].assign_attributes(lesson_params)
         response[:lesson].save!
+        if lesson_params[:name].present?
+          rename_lesson_project(lesson: response[:lesson], name: lesson_params[:name])
+          rename_lesson_remixes(lesson: response[:lesson], name: lesson_params[:name])
+        end
         response
       rescue StandardError => e
         Sentry.capture_exception(e)
         errors = response[:lesson].errors.full_messages.join(',')
         response[:error] = "Error updating lesson: #{errors}"
         response
+      end
+
+      def rename_lesson_project(lesson:, name:)
+        lesson.project.update!(name: name)
+      end
+
+      def rename_lesson_remixes(lesson:, name:)
+        lesson_remixes = Project.where(remixed_from_id: lesson.project.id)
+        lesson_remixes.each do |remix|
+          remix.update!(name: name)
+        end
       end
     end
   end
