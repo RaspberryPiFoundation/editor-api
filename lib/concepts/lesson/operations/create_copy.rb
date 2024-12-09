3de247dfeb2671dad5af0504d8bf032cf36915ac
Lois Wells <lois.wells@raspberrypi.org>
2024-12-09 17:04:51 +0000

    update copy lesson to copy the project tpp

--- lib/concepts/lesson/operations/create_copy.rb
+++ lib/concepts/lesson/operations/create_copy.rb
@@ -19,9 +19,24 @@ class Lesson
 
       # TODO: copy projects
       def build_copy(lesson, lesson_params)
-        copy = Lesson.new(parent: lesson, name: lesson.name, description: lesson.description)
-        copy.assign_attributes(lesson_params)
-        copy
+        lesson_copy = Lesson.new(parent: lesson, name: lesson.name, description: lesson.description)
+        lesson_copy.assign_attributes(lesson_params)
+
+        project_params = lesson.project.attributes.except('id', 'identifier')
+        project_copy = Project.new(project_params)
+        project_copy.user_id = lesson_params[:user_id]
+        project_copy.lesson_id = lesson_copy.id
+
+        lesson.project.images.each do |image|
+          project_copy.images.attach(image.blob)
+        end
+
+        lesson.project.components.each do |component|
+          project_copy.components.build({ name: component.name, extension: component.extension, content: component.content })
+        end
+
+        lesson_copy.project = project_copy
+        lesson_copy
       end
     end
   end
