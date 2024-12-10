b6ec2ac8c2351a5e6742cec4871745da0f861db7
Lois Wells <lois.wells@raspberrypi.org>
2024-12-10 11:47:07 +0000

    testing

--- spec/concepts/lesson/create_copy_spec.rb
+++ spec/concepts/lesson/create_copy_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Lesson::CreateCopy, type: :unit do
   let(:teacher_id) { SecureRandom.uuid }
 
   let!(:lesson) do
-    create(:lesson, name: 'Test Lesson', description: 'Description', user_id: teacher_id)
+    create(:lesson, :with_project_components, :with_project_image, name: 'Test Lesson', description: 'Description', user_id: teacher_id)
   end
 
   let(:lesson_params) do
@@ -52,6 +52,57 @@ RSpec.describe Lesson::CreateCopy, type: :unit do
     new_params = lesson_params.merge(description: 'New Description')
     response = described_class.call(lesson:, lesson_params: new_params)
     expect(response[:lesson].description).to eq('New Description')
+  end
+
+  it 'creates a new project' do
+    expect { described_class.call(lesson:, lesson_params:) }.to change(Project, :count).by(1)
+  end
+
+  it 'gives the project a new identifier' do
+    response = described_class.call(lesson:, lesson_params:)
+    expect(response[:lesson].project.identifier).not_to be_nil
+    expect(response[:lesson].project.identifier).not_to eq(lesson.project.identifier)
+  end
+
+  it 'gives the project the correct name' do
+    response = described_class.call(lesson:, lesson_params:)
+    expect(response[:lesson].project.name).to eq(lesson.project.name)
+  end
+
+  it 'gives the project the correct user_id' do
+    response = described_class.call(lesson:, lesson_params:)
+    expect(response[:lesson].project.user_id).to eq(teacher_id)
+  end
+
+  it 'gives the project the correct lesson_id' do
+    response = described_class.call(lesson:, lesson_params:)
+    expect(response[:lesson].project.lesson_id).to eq(response[:lesson].id)
+  end
+
+  it 'copies the images from the parent project' do
+    response = described_class.call(lesson:, lesson_params:)
+    expect(response[:lesson].project.images.length).to eq(lesson.project.images.length)
+  end
+
+  it 'copies the components from the parent project with the correct name' do
+    original_component = lesson.project.components.first
+    response = described_class.call(lesson:, lesson_params:)
+    copied_component = response[:lesson].project.components.first
+    expect(copied_component.name).to eq(original_component.name)
+  end
+
+  it 'copies the components from the parent project with the correct extension' do
+    original_component = lesson.project.components.first
+    response = described_class.call(lesson:, lesson_params:)
+    copied_component = response[:lesson].project.components.first
+    expect(copied_component.extension).to eq(original_component.extension)
+  end
+
+  it 'copies the components from the parent project with the correct content' do
+    original_component = lesson.project.components.first
+    response = described_class.call(lesson:, lesson_params:)
+    copied_component = response[:lesson].project.components.first
+    expect(copied_component.content).to eq(original_component.content)
   end
 
   context 'when creating a copy fails' do
