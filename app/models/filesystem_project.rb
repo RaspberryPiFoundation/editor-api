8e06146be0ddd71bc569c8a608478b6b2c39e6df
Lois Wells <lois.wells@raspberrypi.org>
2025-01-27 12:41:56 +0000

    refactoring filesystemproject to allow audio and video files

--- app/models/filesystem_project.rb
+++ app/models/filesystem_project.rb
@@ -4,29 +4,41 @@ require 'yaml'
 
 class FilesystemProject
   CODE_FORMATS = ['.py', '.csv', '.txt', '.html', '.css'].freeze
-  IMAGE_FORMATS = ['.png', '.jpg', '.jpeg', '.webp'].freeze
+  # IMAGE_FORMATS = ['.png', '.jpg', '.jpeg', '.webp'].freeze
   PROJECTS_ROOT = Rails.root.join('lib/tasks/project_components')
 
   def self.import_all!
     PROJECTS_ROOT.each_child do |dir|
       proj_config = YAML.safe_load_file(dir.join('project_config.yml').to_s)
       files = dir.children
-      code_files = files.filter { |file| CODE_FORMATS.include? File.extname(file) }
-      image_files = files.filter { |file| IMAGE_FORMATS.include? File.extname(file) }
 
       components = []
-      code_files.each do |file|
-        components << component(file, dir)
-      end
-
       images = []
-      image_files.each do |file|
-        images << image(file, dir)
+      videos = []
+      audio_files = []
+
+      files.each do |file|
+        # skip the project_config.yml file
+        next if file.basename.to_s == 'project_config.yml'
+
+        mime_type = file_mime_type(file)
+
+        if CODE_FORMATS.include? File.extname(file)
+          components << component(file, dir)
+        elsif mime_type =~ /image/
+          images << media(file, dir)
+        elsif mime_type =~ /video/
+          videos << media(file, dir)
+        elsif mime_type =~ /audio/
+          audio_files << media(file, dir)
+        else
+          raise "File #{File.basename(file)} has unsupported file type: #{mime_type}"
+        end
       end
 
       project_importer = ProjectImporter.new(name: proj_config['NAME'], identifier: proj_config['IDENTIFIER'],
                                              type: proj_config['TYPE'] || 'python',
-                                             locale: proj_config['LOCALE'] || 'en', components:, images:)
+                                             locale: proj_config['LOCALE'] || 'en', components:, images:, videos:, audio_files:)
       project_importer.import!
     end
   end
@@ -39,7 +51,11 @@ class FilesystemProject
     { name:, extension:, content: code, default: }
   end
 
-  def self.image(file, dir)
+  def self.file_mime_type(file)
+    Marcel::MimeType.for(File.open(file), name: File.basename(file))
+  end
+
+  def self.media(file, dir)
     filename = File.basename(file)
     io = File.open(dir.join(filename).to_s)
     { filename:, io: }
