b75388d8214e17516ac46ad44df34970e40fea2c
Dan Halson <dan.halson@raspberrypi.org>
2025-02-19 18:06:40 +0000

    506: Tidy up error message

--- app/jobs/upload_job.rb
+++ app/jobs/upload_job.rb
@@ -45,10 +45,7 @@ class UploadJob < ApplicationJob
   def perform(payload)
     modified_locales(payload).each do |locale|
       projects_data = load_projects_data(locale, repository(payload), owner(payload))
-      if projects_data.data.repository&.object.nil?
-        Rails.logger.warn 'Build skipped, does the repo exist?'
-        raise RepositoryNotFound, "The repository could not be found (is it private?): #{repository(payload)}"
-      end
+      raise RepositoryNotFound, "Repository not found (is it private?): #{repository(payload)}" if projects_data.data.repository&.object.nil?
 
       projects_data.data.repository.object.entries.each do |project_dir|
         project = format_project(project_dir, locale, repository(payload), owner(payload))
