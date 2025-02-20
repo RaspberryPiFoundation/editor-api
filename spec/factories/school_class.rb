287cd4a723fec13ed48cf665f3c4d305a675e237
Lois Wells <lois.wells@raspberrypi.org>
2025-02-20 12:30:14 +0000

    class teacher and class student factories

--- spec/factories/school_class.rb
+++ spec/factories/school_class.rb
@@ -2,7 +2,18 @@
 
 FactoryBot.define do
   factory :school_class do
-    teacher_id { SecureRandom.uuid }
+    # teacher_ids { [SecureRandom.uuid] }
     sequence(:name) { |n| "Class #{n}" }
+
+    transient do
+      teacher_ids {[SecureRandom.uuid]}
+    end
+
+    after(:create) do |school_class, evaluator|
+      class_teachers = evaluator.teacher_ids.map do |teacher_id|
+        create(:class_teacher, school_class:, teacher_id:)
+      end
+      school_class.update!(class_teachers:)
+    end
   end
 end
