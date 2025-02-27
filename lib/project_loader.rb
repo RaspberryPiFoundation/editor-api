56022c6f1dbea4ea1f94c95497aab3971e7c3d2d
James Mead <james.mead+rpf@gofreerange.com>
2025-02-27 16:41:11 +0000

    Add scratch-only option for project show endpoint
    We want to be able to load Scratch projects from the Experience CS app
    without breaking anything in the Code Editor app. This is now possible
    by setting `project_type=scratch` in the query string for the
    `Api::Projects#show` action.
    
    We've bumped the maxium number of nested rspec contexts up from 4 -> 5
    which we don't think is too terrible. It would be quite hard to re-work
    the changes in this spec to use less nesting without significant
    restructuring which we want to avoid at the moment.
    
    Co-authored-by: Chris Roos <chris.roos@gofreerange.com>

--- lib/project_loader.rb
+++ lib/project_loader.rb
@@ -8,8 +8,9 @@ class ProjectLoader
     @locales = [*locales, 'en', nil]
   end
 
-  def load(include_images: false)
-    query = Project.where(identifier:, locale: @locales)
+  def load(include_images: false, only_scratch: false)
+    query = only_scratch ? Project.unscoped.where(project_type: 'scratch') : Project
+    query = query.where(identifier:, locale: @locales)
     query = query.includes(images_attachments: :blob) if include_images
     query.min_by { |project| @locales.find_index(project.locale) }
   end
