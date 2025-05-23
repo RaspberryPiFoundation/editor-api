6f20092ed82e24512ce4bfad7019ecbcb0484232
James Mead <james.mead+rpf@gofreerange.com>
2025-05-23 09:07:23 +0100

    Rename project_params in PublicProjectsController
    I'm about to add an update action which needs different params, so
    renaming `project_params` to `create_params` will make that change
    simpler.

--- app/controllers/api/public_projects_controller.rb
+++ app/controllers/api/public_projects_controller.rb
@@ -7,7 +7,7 @@ module Api
 
     def create
       authorize! :create, :public_project
-      result = PublicProject::Create.call(project_hash: project_params)
+      result = PublicProject::Create.call(project_hash: create_params)
 
       if result.success?
         @project = result[:project]
@@ -19,12 +19,12 @@ module Api
 
     private
 
-    def project_params
+    def create_params
       params.require(:project).permit(:identifier, :locale, :project_type, :name)
     end
 
     def restrict_project_type
-      project_type = project_params[:project_type]
+      project_type = create_params[:project_type]
       return if project_type == Project::Types::SCRATCH
 
       raise CanCan::AccessDenied.new("#{project_type} not yet supported", :create, :public_project)
