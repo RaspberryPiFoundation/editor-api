167925402f0e5902401ccbe6539ef5822833494a
James Mead <james.mead+rpf@gofreerange.com>
2025-05-25 16:54:35 +0100

    Fix updating project feature spec
    Previously this spec was not representative of how the API was being
    used in practice and it was only really working by accident. The PUT
    requests in the examples were using the `Project#id` rather than the
    `Project#identifier` as they should have been.
    
    The spec was working even though the
    `Api::ProjectsController#load_project` before action was *not* finding
    the project, because the `load_and_authorize_resource` before action was
    then finding the project using `Project.find(params[:id])` and setting
    the `@project` instance variable used by the rest of the logic in the
    controller.
    
    However, clients of editor-api like editor-standalone use the project
    identifier as the resource ID in the URL path [1], so this spec was
    completely unrepresentative of how the endpoint was really being used.
    
    In this commit I've changed the examples to use the `Project#identifier`
    as the resource ID in the PUT requests. This means that the
    `Api::ProjectsController#load_project` before action now finds the
    project by identifier and since it sets the `@project` instance variable
    the `load_and_authorize_resource` before action no longer attempts to
    load the resource, it just does the authorize step using the already
    loaded project.
    
    In order to make this work reliably, I had to explicitly set the
    `Project#locale` to one of the fallback locales in `ProjectLoader` [2],
    i.e. 'en' or `nil`. Otherwise, the random locale selected in the project
    factory [3] meant that sometimes the `load_project` before action (which
    uses the `ProjectLoader` did not find the project and the
    `load_and_authorize_resource` before action raised an
    `ActiveRecord::RecordNotFound` resulting in a 404 Not Found.
    
    Now the spec is no longer making use of the randome locale from the
    factory, I've taken the opportunity to add some new examples
    demonstrating the behaviour when the project has different locales. This
    effectively demonstrates that `load_project` is wired up to
    `ProjectLoader` correctly.
    
    As an aside, I'm not convinced that having the factory select a locale
    at random is a good idea. I found it very confusing when it led to
    undeterministic specs. However, I'll leave that for another time.
    
    [1]: https://github.com/RaspberryPiFoundation/editor-standalone/blob/1d4375635cb6890794732072d608dbd4b05b3bb0/src/utils/apiCallHandler/projects.js#L16
    [2]: https://github.com/RaspberryPiFoundation/editor-api/blob/b4bd337d09a88b1f41ecdc13136f7d11da0dcf89/lib/project_loader.rb#L8
    [3]: https://github.com/RaspberryPiFoundation/editor-api/blob/b4bd337d09a88b1f41ecdc13136f7d11da0dcf89/spec/factories/project.rb#L9

--- spec/features/project/updating_a_project_spec.rb
+++ spec/features/project/updating_a_project_spec.rb
@@ -5,7 +5,8 @@ require 'rails_helper'
 RSpec.describe 'Updating a project', type: :request do
   let(:project_type) { Project::Types::PYTHON }
   let(:user_id) { owner.id }
-  let!(:project) { create(:project, name: 'Test Project', user_id:, locale: 'en', project_type:) }
+  let(:locale) { 'en' }
+  let!(:project) { create(:project, name: 'Test Project', user_id:, locale:, project_type:) }
   let(:owner) { create(:owner, school:) }
   let(:school) { create(:school) }
 
@@ -27,31 +28,31 @@ RSpec.describe 'Updating a project', type: :request do
   end
 
   it 'responds 200 OK' do
-    put("/api/projects/#{project.id}", headers:, params:)
+    put("/api/projects/#{project.identifier}", headers:, params:)
     expect(response).to have_http_status(:ok)
   end
 
   it 'responds with the project JSON' do
-    put("/api/projects/#{project.id}", headers:, params:)
+    put("/api/projects/#{project.identifier}", headers:, params:)
     data = JSON.parse(response.body, symbolize_names: true)
 
     expect(data[:name]).to eq('New Name')
   end
 
   it 'responds with the components JSON' do
-    put("/api/projects/#{project.id}", headers:, params:)
+    put("/api/projects/#{project.identifier}", headers:, params:)
     data = JSON.parse(response.body, symbolize_names: true)
 
     expect(data[:components].first[:content]).to eq('print("hello")')
   end
 
   it 'responds 422 Unprocessable Entity when params are invalid' do
-    put("/api/projects/#{project.id}", headers:, params: { project: { components: [{ name: ' ' }] } })
+    put("/api/projects/#{project.identifier}", headers:, params: { project: { components: [{ name: ' ' }] } })
     expect(response).to have_http_status(:unprocessable_entity)
   end
 
   it 'responds 401 Unauthorized when no token is given' do
-    put("/api/projects/#{project.id}", params:)
+    put("/api/projects/#{project.identifier}", params:)
     expect(response).to have_http_status(:unauthorized)
   end
 
@@ -75,6 +76,29 @@ RSpec.describe 'Updating a project', type: :request do
       data = JSON.parse(response.body, symbolize_names: true)
 
       expect(data[:name]).to eq('Test Project')
+    end
+  end
+
+  context 'when locale is nil, i.e. the other fallback locale in ProjectLoader' do
+    let(:locale) { nil }
+
+    it 'responds 200 OK even though no locale is specified in query string' do
+      put("/api/projects/#{project.identifier}", headers:, params:)
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  context "when locale is 'fr', i.e. not a fallback locale in ProjectLoader" do
+    let(:locale) { 'fr' }
+
+    it 'responds 200 OK if locale is specified in query string' do
+      put("/api/projects/#{project.identifier}?locale=fr", headers:, params:)
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'responds 404 Not Found if locale is not specified in query string' do
+      put("/api/projects/#{project.identifier}", headers:, params:)
+      expect(response).to have_http_status(:not_found)
     end
   end
 
