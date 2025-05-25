dca4c4b8ea844a3318c48de5e9ac030d63455864
James Mead <james.mead+rpf@gofreerange.com>
2025-05-25 10:02:51 +0100

    Improve Project::Create request spec
    * Remove redundant `create_project_with_content`. I suspect this was
      introduced due to a misunderstanding of how `subject` & `let` blocks
      are evaluated. The relevant difference is achieved through a variation
      of the `project_hash` `let` block in the two `context` blocks.
    * Move `subject`, `let` & `before` blocks into the `describe` or
      `context` blocks where they make the most sense.
    * Consistently use `Hash` vs `ActionController::Parameters`. Even though
      the latter is more realistic, it seems like an unnecessary
      complication.
    * Remove unneccessary call to
      `ActionController::Parameters.permit_all_parameters = true`. This was
      introduced in this commit [1] without explanation.

--- spec/concepts/project/create_spec.rb
+++ spec/concepts/project/create_spec.rb
@@ -3,21 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe Project::Create, type: :unit do
-  subject(:create_project) { described_class.call(project_hash:) }
-
-  let(:user_id) { 'e0675b6c-dc48-4cd6-8c04-0f7ac05af51a' }
-
-  before do
-    mock_phrase_generation
-    ActionController::Parameters.permit_all_parameters = true
-  end
-
   describe '.call' do
-    let(:project_hash) { ActionController::Parameters.new({}).merge(user_id:) }
+    subject(:create_project) { described_class.call(project_hash:) }
+
+    let(:user_id) { 'e0675b6c-dc48-4cd6-8c04-0f7ac05af51a' }
+
+    before do
+      mock_phrase_generation
+    end
 
     context 'with valid content' do
-      subject(:create_project_with_content) { described_class.call(project_hash:) }
-
       let(:project_hash) do
         {
           project_type: Project::Types::PYTHON,
@@ -32,16 +27,18 @@ RSpec.describe Project::Create, type: :unit do
       end
 
       it 'returns success' do
-        expect(create_project_with_content.success?).to be(true)
+        expect(create_project.success?).to be(true)
       end
 
       it 'returns project with correct component content' do
-        new_project = create_project_with_content[:project]
+        new_project = create_project[:project]
         expect(new_project.components.first.content).to eq('print("hello world")')
       end
     end
 
     context 'when creation fails' do
+      let(:project_hash) { { user_id: } }
+
       before do
         mock_project = instance_double(Project)
         allow(mock_project).to receive(:components).and_raise('Some error')
