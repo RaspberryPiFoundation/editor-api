2a6a91caad5a11200a2a58bbfa61de3f9a2460e0
Lois Wells <88904316+loiswells97@users.noreply.github.com>
2025-07-30 12:20:50 +0100

    Enable reseed on test API (#572)
    Removes the env check so we can run this on the test API which has Rails
    env set to production. There are other safeguards in place such as an
    API key and host check that should prevent this from actually running on
    production.

--- app/controllers/test_utilities_controller.rb
+++ app/controllers/test_utilities_controller.rb
@@ -9,8 +9,6 @@ class TestUtilitiesController < ApplicationController
     pp 'api_key_valid?', api_key_valid?
     pp 'api_key_present?', ENV['RESEED_API_KEY'].present?
     pp 'api_key_correct?', request.headers['X-RESEED-API-KEY'] == ENV['RESEED_API_KEY']
-    pp 'environment_allowed?', environment_allowed?
-    pp 'environment', Rails.env
     pp 'host_allowed?', host_allowed?
     pp 'host', request.host
     # rubocop:enable Rails/Output
@@ -28,15 +26,11 @@ class TestUtilitiesController < ApplicationController
   private
 
   def reseed_allowed?
-    api_key_valid? && environment_allowed? && host_allowed?
+    api_key_valid? && host_allowed?
   end
 
   def api_key_valid?
     ENV['RESEED_API_KEY'].present? && request.headers['X-RESEED-API-KEY'] == ENV['RESEED_API_KEY']
-  end
-
-  def environment_allowed?
-    Rails.env.development? || Rails.env.test?
   end
 
   def host_allowed?
