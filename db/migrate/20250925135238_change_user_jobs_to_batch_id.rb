0eb4a08c725da262d4965c633dd24a3c8134321f
Fraser Speirs <fraser@speirs.org>
2025-10-07 14:39:47 +0100

    Reverse the removal of the good_job_id column from the user_jobs table.
    This should allow existing jobs to continue working until completed.

--- db/migrate/20250925135238_change_user_jobs_to_batch_id.rb
+++ db/migrate/20250925135238_change_user_jobs_to_batch_id.rb
@@ -1,6 +1,5 @@
 class ChangeUserJobsToBatchId < ActiveRecord::Migration[7.2]
   def change
     add_column :user_jobs, :good_job_batch_id, :uuid
-    remove_column :user_jobs, :good_job_id, :uuid
   end
 end
