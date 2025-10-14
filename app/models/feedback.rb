1dc0f11cfe8023ce5136d2fc5cb633771f070977
Lois Wells <lois.wells@raspberrypi.org>
2025-10-14 11:57:06 +0100

    improving feedback validations and adding factory and model tests

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -6,6 +6,8 @@ class Feedback < ApplicationRecord
   validates :user_id, presence: true
   validates :school_project, presence: true
   validate :user_has_the_school_owner_or_school_teacher_role_for_the_school
+  validate :parent_project_belongs_to_lesson
+  validate :parent_project_belongs_to_school_class
   validate :user_is_the_class_teacher_for_the_school_project
 
   def user_has_the_school_owner_or_school_teacher_role_for_the_school
@@ -20,9 +22,24 @@ class Feedback < ApplicationRecord
     errors.add(:user, msg)
   end
 
+  def parent_project_belongs_to_lesson
+    parent_project = school_project&.project&.parent
+    return if parent_project&.lesson_id.present?
+    msg = "Parent project '#{parent_project&.id}' does not belong to a 'lesson'"
+    errors.add(:user, msg)
+  end
+
+  def parent_project_belongs_to_school_class
+    parent_project = school_project&.project&.parent
+    return if parent_project&.lesson&.school_class_id.present?
+
+    msg = "Parent project '#{parent_project&.id}' does not belong to a 'school-class'"
+    errors.add(:user, msg)
+  end
+
   def user_is_the_class_teacher_for_the_school_project
     school_class = school_project&.project&.parent&.lesson&.school_class
-    return if !school_class || school_class.teacher_ids.include?(user_id)
+    return if !school_project || school_class&.teacher_ids&.include?(user_id)
 
     errors.add(:user, "'#{user_id}' is not the 'school-teacher' for school_project '#{school_project.id}'")
   end
---
+++ spec/factories/feedback.rb
@@ -0,0 +1,19 @@
+# # frozen_string_literal: true
+
+FactoryBot.define do
+  factory :feedback do
+    content { Faker::Lorem.sentence }
+    user_id { teacher.id }
+    school_project { create(:school_project, school: school, project: student_project) }
+
+    transient do
+      school { create(:school) }
+      teacher { create(:teacher, school: school) }
+      student { create(:student, school: school) }
+      school_class { create(:school_class, school: school, teacher_ids: [teacher.id]) }
+      class_student { create(:class_student, school_class: school_class, student_id: student.id) }
+      parent_project { create(:project, user_id: teacher.id, school: school, lesson: create(:lesson, school_class: school_class, user_id: teacher.id)) }
+      student_project { create(:project, parent: parent_project, user_id: student.id) }
+    end
+  end
+end
