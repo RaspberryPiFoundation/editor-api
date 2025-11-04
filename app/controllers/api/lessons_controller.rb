fff6c4cf436dfaf6f054aa87d7c9ffb4e25dfdf1
Lois Wells <lois.wells@raspberrypi.org>
2025-11-04 17:28:00 +0000

    add ordering to ensure it always gets the first record

--- app/controllers/api/lessons_controller.rb
+++ app/controllers/api/lessons_controller.rb
@@ -12,7 +12,11 @@ module Api
       ordered_scope = scope.order(created_at: :asc)
       lessons_with_users = ordered_scope.accessible_by(current_ability).with_users
       remixes = ordered_scope.map do |lesson|
-        lesson.project.remixes.where(user_id: current_user.id).accessible_by(current_ability).first
+        lesson.project.remixes
+          .where(user_id: current_user.id)
+          .accessible_by(current_ability)
+          .order(created_at: :asc)
+          .first
       end
       @lessons_with_users_and_remixes = lessons_with_users.zip(remixes)
       render :index, formats: [:json], status: :ok
