e768ca976d5146d7a603a925d9d52d5a976c8b2f
Lois Wells <lois.wells@raspberrypi.org>
2025-11-07 10:56:23 +0000

    fixing

--- app/controllers/api/lessons_controller.rb
+++ app/controllers/api/lessons_controller.rb
@@ -10,8 +10,11 @@ module Api
       archive_scope = params[:include_archived] == 'true' ? Lesson : Lesson.unarchived
       scope = params[:school_class_id] ? archive_scope.where(school_class_id: params[:school_class_id]) : archive_scope
       ordered_scope = scope.order(created_at: :asc)
-      lessons_with_users = ordered_scope.accessible_by(current_ability).with_users
-      remixes = user_remixes(ordered_scope)
+      accessible_lessons = ordered_scope.accessible_by(current_ability)
+      lessons_with_users = accessible_lessons.with_users
+      remixes = user_remixes(accessible_lessons)
+      pp lessons_with_users
+      pp remixes
       @lessons_with_users_and_remixes = lessons_with_users.zip(remixes)
       render :index, formats: [:json], status: :ok
     end
