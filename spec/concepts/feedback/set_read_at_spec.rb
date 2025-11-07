27bd784c94b084bac7b0b99631cedc11d695971c
Jamie Benstead <jamie.benstead@gmail.com>
2025-11-07 15:27:29 +0000

    Add tests around when set_read fails

--- spec/concepts/feedback/set_read_at_spec.rb
+++ spec/concepts/feedback/set_read_at_spec.rb
@@ -27,5 +27,32 @@ RSpec.describe Feedback::SetRead, type: :unit do
         expect(response[:feedback].read_at).to be_a(ActiveSupport::TimeWithZone)
       end
     end
+
+    context 'when set_read fails' do
+      before do
+        allow(feedback).to receive(:save!).and_raise(StandardError, 'Transition failed')
+      end
+
+      it 'returns a failed operation response' do
+        response = described_class.call(feedback: feedback)
+        expect(response.success?).to be(false)
+      end
+
+      it 'returns the original feedback' do
+        response = described_class.call(feedback: feedback)
+        expect(response[:feedback]).to eq(feedback)
+      end
+
+      it 'does not persist read_at' do
+        described_class.call(feedback: feedback)
+        feedback.reload
+        expect(feedback.read_at).to be_nil
+      end
+
+      it 'includes an error object in the response' do
+        response = described_class.call(feedback: feedback)
+        expect(response[:error]).to be_a(ActiveModel::Errors)
+      end
+    end
   end
 end
