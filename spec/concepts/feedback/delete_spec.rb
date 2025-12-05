7bc9816cc7e647593a0e9332d5f95a1b911d39e7
Lois Wells <lois.wells@raspberrypi.org>
2025-12-05 17:01:02 +0000

    tidying tests

--- spec/concepts/feedback/delete_spec.rb
+++ spec/concepts/feedback/delete_spec.rb
@@ -4,17 +4,17 @@ require 'rails_helper'
 
 RSpec.describe Feedback::Delete, type: :unit do
   let(:feedback) { create(:feedback) }
-  let(:feedback_id) { feedback.id }
 
   describe '.call' do
     context 'when deletion is successful' do
+      let!(:feedback_id) { feedback.id }
+
       it 'returns a successful operation response' do
         response = described_class.call(feedback_id:)
         expect(response.success?).to be(true)
       end
 
       it 'deletes the feedback' do
-        feedback_id = feedback.id
         expect { described_class.call(feedback_id:) }.to change(Feedback, :count).by(-1)
         expect(Feedback.where(id: feedback_id)).to be_empty
       end
