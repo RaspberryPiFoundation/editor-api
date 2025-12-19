f0856d5b36cdbdda39f23155100971187513e932
Dan Halson <dan.halson@raspberrypi.org>
2025-12-19 10:53:10 +0000

    1069: Add error_type to duplication response

--- app/controllers/api/schools_controller.rb
+++ app/controllers/api/schools_controller.rb
@@ -22,7 +22,10 @@ module Api
         @school = result[:school]
         render :show, formats: [:json], status: :created
       else
-        render json: { error: result[:error] }, status: :unprocessable_entity
+        render json: {
+          error: result[:error],
+          error_type: result[:error_type] || :unknown_error
+        }, status: :unprocessable_entity
       end
     end
 
