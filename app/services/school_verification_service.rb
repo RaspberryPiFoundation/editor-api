c03908e64ad6601394b88c9cdb034d2bd2437235
Dan Halson <dan.halson@raspberrypi.org>
2026-01-06 17:04:30 +0000

    1069: Ensure we're rolling back based on the result of onboarding to mimic current behaviour

--- app/services/school_verification_service.rb
+++ app/services/school_verification_service.rb
@@ -8,18 +8,23 @@ class SchoolVerificationService
   end
 
   def verify(token: nil)
+    success = false
     School.transaction do
       school.verify!
 
-      # TODO: Remove this line once the feature flag is retired
-      SchoolOnboardingService.new(school).onboard(token: token) unless FeatureFlags.immediate_school_onboarding?
+      # TODO: Remove this line, once the feature flag is retired
+      success = FeatureFlags.immediate_school_onboarding? || SchoolOnboardingService.new(school).onboard(token: token)
+
+      # TODO: Remove this line, once the feature flag is retired
+      raise ActiveRecord::Rollback unless success
     end
   rescue StandardError => e
     Sentry.capture_exception(e)
     Rails.logger.error { "Failed to verify school #{@school.id}: #{e.message}" }
     false
   else
-    true
+    # TODO: Return 'true', once the feature flag is retired
+    success
   end
 
   delegate :reject, to: :school
