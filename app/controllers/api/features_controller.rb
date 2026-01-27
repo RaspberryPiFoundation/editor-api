09bf0acb71b79466ca66ff610abbafbcb43e5ce5
Matthew Trew <matthew.trew@raspberrypi.org>
2026-01-27 16:59:57 +0100

    Enable school-level flags for logged-in users

--- app/controllers/api/features_controller.rb
+++ app/controllers/api/features_controller.rb
@@ -3,10 +3,8 @@
 module Api
   class FeaturesController < ApiController
     def index
-      Rails.logger.info "Features: #{Flipper.features}"
       features = Flipper.features.map do |feature|
-        # todo: reveal only features that are enabled.
-        [feature.key, Flipper.enabled?(feature.key, current_user)]
+        [feature.key, Flipper.enabled?(feature.key, current_user&.schools&.first)]
       end
 
       render json: features.to_h
--- spec/requests/api/features_spec.rb
+++ spec/requests/api/features_spec.rb
@@ -3,18 +3,76 @@
 require 'rails_helper'
 
 RSpec.describe "Features", type: :request do
+  let(:headers) { { Authorization: UserProfileMock::TOKEN } }
+  let!(:school_class) { create(:school_class, teacher_ids: [teacher.id], school:) }
+  let(:school) { create(:school) }
+  let(:student) { create(:student, school:) }
+  let(:teacher) { create(:teacher, school:) }
 
   describe "Feature flag API" do
-    it "returns a globally-enabled feature as enabled" do
+    it "returns a globally-disabled feature as disabled" do
       # Arrange
-      Flipper.enable :some_cool_feature
+      Flipper.disable :some_global_feature
 
       # Act
       get "/api/features"
 
       # Assert
-      expect(response.body).to include('"some_cool_feature":true')
+      expect(response.body).not_to include('"some_global_feature":true')
     end
+
+    it "returns a globally-enabled feature as enabled" do
+      # Arrange
+      Flipper.enable :some_global_feature
+
+      # Act
+      get "/api/features"
+
+      # Assert
+      expect(response.body).to include('"some_global_feature":true')
+    end
+
+    it "returns a school-level feature as disabled for logged-out user" do
+      # Arrange
+      Flipper.enable_actor :some_school_level_feature, school
+
+      # Act
+      get "/api/features"
+
+      # Assert
+      expect(response.body).not_to include('"some_school_level_feature":true')
+    end
+
+    it "returns a school-level feature as enabled for a student in that school" do
+      # Arrange
+      authenticated_in_hydra_as(student)
+
+      Flipper.enable_actor :some_school_level_feature, school
+
+      # Act
+      get "/api/features", headers: headers
+
+      # Assert
+      expect(response.body).to include('"some_school_level_feature":true')
+    end
+
+    it "returns both school-level and global features as enabled for a student in a school" do
+      # Arrange
+      authenticated_in_hydra_as(student)
+
+      Flipper.enable_actor :some_school_level_feature, school
+      Flipper.enable :some_global_feature
+
+      # Act
+      get "/api/features", headers: headers
+
+      # Assert
+      expect(response.body).to include('"some_school_level_feature":true')
+      expect(response.body).to include('"some_global_feature":true')
+    end
+
+    # todo: Don't leak the existence of disabled feature flags
+
   end
 
   describe "Feature flag web interface" do
@@ -25,5 +83,8 @@ RSpec.describe "Features", type: :request do
       # Assert
       expect(response).to have_http_status(:success)
     end
+
+    # todo: Only admins should be able to access UI
+
   end
 end
