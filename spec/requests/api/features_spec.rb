1d363eb8789ee4af4063339a5fbb961029055bb8
Matthew Trew <matthew.trew@raspberrypi.org>
2026-02-03 11:59:27 +0000

    Move Flipper auth tests to new file

--- spec/requests/api/features_spec.rb
+++ spec/requests/api/features_spec.rb
@@ -72,47 +72,4 @@ RSpec.describe 'Features' do
       expect(response.body).to include('some_global_feature')
     end
   end
-
-  describe 'Feature flag web interface' do
-    it 'is hidden from unauthenticated users' do
-      # Act
-      get '/admin/flipper/features'
-
-      # Assert
-      expect(response).to have_http_status(:not_found)
-    end
-
-    it 'is hidden from student users' do
-      # Arrange
-      sign_in student
-
-      # Act
-      get '/admin/flipper/features'
-
-      # Assert
-      expect(response).to have_http_status(:not_found)
-    end
-
-    it 'is hidden from teacher users' do
-      # Arrange
-      sign_in teacher
-
-      # Act
-      get '/admin/flipper/features'
-
-      # Assert
-      expect(response).to have_http_status(:not_found)
-    end
-
-    it 'is visible to admins' do
-      # Arrange
-      sign_in admin_user
-
-      # Act
-      get '/admin/flipper/features'
-
-      # Assert
-      expect(response).to have_http_status(:success)
-    end
-  end
 end
