cb8e1ff35bf4cd01db819220bdcb14adfece7e8c
Chris Zetter <253059100+zetter-rpf@users.noreply.github.com>
2026-02-16 09:22:18 +0000

    Allow us to configure web concurrency
    Part of https://github.com/RaspberryPiFoundation/digital-editor-issues/issues/1133
    
    We're currently running with a single process (puma web worker) which may mean a single long running request can block our server and cause other requests to fail.
    
    Now we have upgraded to a performance dyno, we have enough RAM to run web workers (Heroku recommends using 2). I've make this configurable (starting with 1) so that it's easier to change (which we would need to do if we are RAM constrained).

--- config/puma.rb
+++ config/puma.rb
@@ -26,20 +26,25 @@ environment ENV.fetch('RAILS_ENV', 'development')
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch('PIDFILE', 'tmp/pids/server.pid')
 
-# Specifies the number of `workers` to boot in clustered mode.
-# Workers are forked web server processes. If using threads and workers together
-# the concurrency of the application would be max `threads` * `workers`.
-# Workers do not work on JRuby or Windows (both of which do not support
-# processes).
-#
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+worker_count = ENV.fetch('WEB_CONCURRENCY', 1).to_i
 
-# Use the `preload_app!` method when specifying a `workers` number.
-# This directive tells Puma to first boot the application and load code
-# before forking the application. This takes advantage of Copy On Write
-# process behavior so workers use less memory.
-#
-# preload_app!
+if worker_count > 1
+
+  # Specifies the number of `workers` to boot in clustered mode.
+  # Workers are forked web server processes. If using threads and workers together
+  # the concurrency of the application would be max `threads` * `workers`.
+  # Workers do not work on JRuby or Windows (both of which do not support
+  # processes).
+
+  workers worker_count
+
+  # Use the `preload_app!` method when specifying a `workers` number.
+  # This directive tells Puma to first boot the application and load code
+  # before forking the application. This takes advantage of Copy On Write
+  # process behavior so workers use less memory.
+
+  preload_app!
+end
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
