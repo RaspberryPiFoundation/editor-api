e4b72f8d73dbf7761bb2a1e933830f97ee3edfa6
Chris Zetter <253059100+zetter-rpf@users.noreply.github.com>
2026-03-09 15:07:15 +0000

    Move encryption config to application.rb so it's set earlier
    Upgrading flipper caused the tests to break because when it triggered the application record encyption settings to be initialized before they were configured.
    
    Rails suggests putting this config into the application.rb[1], which ensures it is loaded earlier and avoids this problem.
    
    I scan the flipper changes and it's not obvious what caused this, but it's likely to do with changes in interaction with active record.
    
    Alternatively we could set flipper to 'require: false',.
    
    [1] - https://guides.rubyonrails.org/active_record_encryption.html

--- config/application.rb
+++ config/application.rb
@@ -60,5 +60,9 @@ module App
 
     config.middleware.insert_before 0, CorpMiddleware
     config.generators.system_tests = nil
+
+    config.active_record.encryption.primary_key = ENV.fetch('ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY')
+    config.active_record.encryption.deterministic_key = ENV.fetch('ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY')
+    config.active_record.encryption.key_derivation_salt = ENV.fetch('ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT')
   end
 end
