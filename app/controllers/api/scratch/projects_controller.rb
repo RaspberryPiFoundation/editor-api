5fa8d5457a106a1c6a6e6f9b891b13a3a6085d9b
Chris Zetter <253059100+zetter-rpf@users.noreply.github.com>
2026-03-12 14:57:35 +0000

    Scratch projects create action for remixing
    This is based on the similar endpoint in Experience CS.
    
    Having this will allowed me to test the behaviour of Scratch remixing locally and it will allow us to start work implementing remixing on the frontend before the API is fully implemented.
    
    I considered re-using our existing endpoint for remixing, but this is not possible because Scratch does not support updating an unsaved project's id (see [1] for more).
    
    [1] - https://github.com/RaspberryPiFoundation/digital-editor-issues/issues/1189#issuecomment-4047292796

--- app/controllers/api/scratch/projects_controller.rb
+++ app/controllers/api/scratch/projects_controller.rb
@@ -6,12 +6,26 @@ module Api
       skip_before_action :authorize_user, only: [:show]
       skip_before_action :check_scratch_feature, only: [:show]
 
+      before_action :ensure_create_is_a_remix, only: %i[create]
+
       def show
         render :show, formats: [:json]
       end
 
+      def create
+        render json: { status: 'ok', 'content-name': 'new-project-id' }, status: :ok
+      end
+
       def update
         render json: { status: 'ok' }, status: :ok
+      end
+
+      private
+
+      def ensure_create_is_a_remix
+        return if params[:is_remix] == '1'
+
+        render json: { error: 'Only remixing existing projects is allowed' }, status: :forbidden
       end
     end
   end
