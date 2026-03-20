0a89b75de2acfad3982604723ba5f0546c2c4110
Fraser Speirs <fraser.speirs@raspberrypi.org>
2026-03-20 11:58:51 +0000

    Add Salesforce sync base infrastructure

---
+++ lib/feature_flags.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module FeatureFlags
+  def self.immediate_school_onboarding?
+    ENV['ENABLE_IMMEDIATE_SCHOOL_ONBOARDING'] == 'true'
+  end
+
+  def self.salesforce_sync?
+    ENV['SALESFORCE_ENABLED'] != 'false'
+  end
+end
