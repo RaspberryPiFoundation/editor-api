6adf513940587fce8e080e82c4a93749ff07d0fe
Fraser Speirs <fraser.speirs@raspberrypi.org>
2026-03-20 11:59:47 +0000

    Implement Contact sync to Salesforce

---
+++ app/jobs/salesforce/contact_sync_job.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Salesforce
+  class ContactSyncJob < SalesforceSyncJob
+    MODEL_CLASS = Salesforce::Contact
+
+    def perform(school_id:)
+      school = ::School.find(school_id)
+
+      sf_contact = Salesforce::Contact.find_by(pi_accounts_unique_id__c: school.creator_id)
+      raise SalesforceRecordNotFound, "Contact not found for creator_id: #{school.creator_id}" unless sf_contact
+
+      sf_contact.experiencecsagreetouxcontact__c = school.creator_agree_to_ux_contact
+      sf_contact.save!
+    end
+  end
+end
