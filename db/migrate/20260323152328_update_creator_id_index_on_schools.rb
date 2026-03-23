d5b80e74fec4e661898adcc89464c0a086b6cac6
Jamie Benstead <jamie.benstead@gmail.com>
2026-03-23 15:36:37 +0000

    Change migration to be reversible if needed

--- db/migrate/20260323152328_update_creator_id_index_on_schools.rb
+++ db/migrate/20260323152328_update_creator_id_index_on_schools.rb
@@ -1,5 +1,5 @@
 class UpdateCreatorIdIndexOnSchools < ActiveRecord::Migration[7.2]
-  def change
+  def up
     remove_index :schools, name: "index_schools_on_creator_id"
 
     add_index :schools,
@@ -7,5 +7,14 @@ class UpdateCreatorIdIndexOnSchools < ActiveRecord::Migration[7.2]
               unique: true,
               where: "rejected_at IS NULL",
               name: "index_schools_on_creator_id_active_only"
+  end
+
+  def down
+    remove_index :schools, name: "index_schools_on_creator_id_active_only"
+
+    add_index :schools,
+              :creator_id,
+              unique: true,
+              name: "index_schools_on_creator_id"
   end
 end
