f53a903d7ea48d731458e6f42fa7848b2de16049
Fraser Speirs <fraser.speirs@raspberrypi.org>
2026-03-25 10:49:32 +0000

    Remove accidentally included immediate onboarding feature flag

--- lib/feature_flags.rb
+++ lib/feature_flags.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 module FeatureFlags
-  def self.immediate_school_onboarding?
-    ENV['ENABLE_IMMEDIATE_SCHOOL_ONBOARDING'] == 'true'
-  end
-
   def self.salesforce_sync?
     ENV['SALESFORCE_ENABLED'] == 'true'
   end
