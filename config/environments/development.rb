bfbb1b46f9e5596bf1d019dac64c71d4afaeef13
Chris Zetter <253059100+zetter-rpf@users.noreply.github.com>
2026-03-27 16:49:45 +0000

    Make the rails log level configurable in development
    When running rake tasks locally there can be a lot of output from SQL queries and active storage, making it harder to see warnings.
    Make it possible to configure the Rails log level in development using environment variables.
    This is just like the code in production.rb except it uses the development default of debug.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,6 +55,8 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
+  config.log_level = ENV.fetch('RAILS_LOG_LEVEL', 'debug')
+
   # Raise exceptions for disallowed deprecations.
   config.active_support.disallowed_deprecation = :raise
 
