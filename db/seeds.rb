1daeec9750f46d7137f3feeab5e4a76dfec8c2cf
Fraser Speirs <fraser@speirs.org>
2026-03-31 09:36:12 +0100

    Automatically seed Experience CS Projects
    This is a small quality-of-life update for Experience CS developers to
    automatically invoke the rake task 'projects:create_experience_cs_examples' when editor-api is seeded.
    
    This will save excs developers having to (a) know and (b) remember to
    invoke this task when resetting the database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,5 @@ require 'rake'
 if Rails.env.development?
   Rake::Task['projects:create_all'].invoke
   Rake::Task['for_education:seed_a_school_with_lessons_and_students'].invoke
+  Rake::Task['projects:create_experience_cs_examples'].invoke
 end
