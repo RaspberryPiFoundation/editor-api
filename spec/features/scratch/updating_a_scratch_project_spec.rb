8ed2e84d9e90a63e01d4c08e0c561f741f39ec3b
acampo <261805581+abcampo-iry@users.noreply.github.com>
2026-04-09 10:48:30 +0200

    Use headers instead of cookies for authorization (#771)
    ## Status
    
    - Closes
    [1256](https://github.com/RaspberryPiFoundation/digital-editor-issues/issues/1256)
    - Related to
    [1239](https://github.com/RaspberryPiFoundation/digital-editor-issues/issues/1239)
    and
    [1255](https://github.com/RaspberryPiFoundation/digital-editor-issues/issues/1255)
    
    ## What's changed?
    
    - Force endpoints to use authentication headers instead of Cookies
    - Removed cookie helpers since not used anymore
    
    ## Steps to perform after deploying to production
    
    _If the production environment requires any extra work after this PR has
    been deployed detail it here. This could be running a Rake task, a
    migration, or upgrading a Gem. That kind of thing._

--- spec/features/scratch/updating_a_scratch_project_spec.rb
+++ spec/features/scratch/updating_a_scratch_project_spec.rb
@@ -5,14 +5,14 @@ require 'rails_helper'
 RSpec.describe 'Updating a Scratch project', type: :request do
   let(:school) { create(:school) }
   let(:teacher) { create(:teacher, school:) }
-  let(:cookie_headers) { { 'Cookie' => "scratch_auth=#{UserProfileMock::TOKEN}" } }
+  let(:auth_headers) { { 'Authorization' => UserProfileMock::TOKEN } }
 
   before do
     Flipper.disable :cat_mode
     Flipper.disable_actor :cat_mode, school
   end
 
-  it 'responds 401 Unauthorized when no cookie is provided' do
+  it 'responds 401 Unauthorized when no Authorization header is provided' do
     put '/api/scratch/projects/any-identifier', params: { project: { targets: [] } }
 
     expect(response).to have_http_status(:unauthorized)
@@ -21,13 +21,12 @@ RSpec.describe 'Updating a Scratch project', type: :request do
   it 'responds 404 Not Found when cat_mode is not enabled' do
     authenticated_in_hydra_as(teacher)
 
-    put '/api/scratch/projects/any-identifier', params: { content: { targets: [] } }, headers: cookie_headers
+    put '/api/scratch/projects/any-identifier', params: { content: { targets: [] } }, headers: auth_headers
 
     expect(response).to have_http_status(:not_found)
   end
 
-  it 'updates a project when cat_mode is enabled and a cookie is provided' do
-    # Arrange
+  it 'updates a project when cat_mode is enabled and an Authorization header is provided' do
     authenticated_in_hydra_as(teacher)
     Flipper.enable_actor :cat_mode, school
     project = create(
@@ -37,10 +36,8 @@ RSpec.describe 'Updating a Scratch project', type: :request do
     )
     create(:scratch_component, project: project)
 
-    # Act
-    put "/api/scratch/projects/#{project.identifier}", params: { targets: ['some update'] }, headers: cookie_headers
+    put "/api/scratch/projects/#{project.identifier}", params: { targets: ['some update'] }, headers: auth_headers
 
-    # Assert
     expect(response).to have_http_status(:ok)
 
     data = JSON.parse(response.body, symbolize_names: true)
