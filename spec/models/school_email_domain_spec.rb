269aafa6c6ac7a98b4790e224df3b9ba7b841956
Pete Simonovic <69108995+PetarSimonovic@users.noreply.github.com>
2026-04-21 15:39:18 +0100

    Declare subject above other let statements

--- spec/models/school_email_domain_spec.rb
+++ spec/models/school_email_domain_spec.rb
@@ -3,9 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe SchoolEmailDomain do
-  let(:school) { create(:school, creator_id: SecureRandom.uuid) }
-
   subject { described_class.new(school:, domain: 'example.edu') }
+
+  let(:school) { create(:school, creator_id: SecureRandom.uuid) }
 
   it { is_expected.to belong_to(:school) }
   it { is_expected.to validate_presence_of(:domain) }
