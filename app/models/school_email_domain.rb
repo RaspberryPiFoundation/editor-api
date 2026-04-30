afaf94173fea5533b0a244a416ea090ceaebd81b
Pete Simonovic <69108995+PetarSimonovic@users.noreply.github.com>
2026-04-30 17:31:58 +0100

    Align host validation with Profile

--- app/models/school_email_domain.rb
+++ app/models/school_email_domain.rb
@@ -18,14 +18,25 @@ class SchoolEmailDomain < ApplicationRecord
     value = "http://#{value}" unless %r{\A[a-z][a-z0-9+\-.]*://}i.match?(value)
     uri = URI.parse(value)
     host = uri.host&.delete_suffix('.')
-    return if host.blank?
+
+    validate_host(host)
+  rescue URI::InvalidURIError
+    errors.add(:domain, :invalid)
+  end
+
+  def validate_host(host)
+    accounts_host_format =
+      /\A\s*(?:[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?\.)+[A-Za-z]{2,63}\s*\z/i
+
+    unless host&.match?(accounts_host_format)
+      errors.add(:domain, :invalid)
+      return
+    end
 
     if PublicSuffix.valid?(host)
       self.domain = host
     else
       errors.add(:domain, :invalid)
     end
-  rescue URI::InvalidURIError
-    errors.add(:domain, :invalid)
   end
 end
