5671fb6db382088daa741e943c1d56e3aa41ae00
Chris Zetter <253059100+zetter-rpf@users.noreply.github.com>
2026-05-21 16:24:17 +0100

    Also save assets to bucket so they can be served from CDN
    Many assets from the asset library are rendered on the page at once which will cause a lot of requests to editor API.
    
    This is being stored in the editor-assets r2 bucket. Note that region isn't used by has to be set to a valid value.

--- lib/scratch_asset_importer.rb
+++ lib/scratch_asset_importer.rb
@@ -32,15 +32,67 @@ class ScratchAssetImporter
   end
 
   def import
-    return if ScratchAsset.global_assets.exists?(filename: asset_name)
-
-    sleep(ASSET_FETCHING_DELAY)
-    asset = connection.get("#{asset_name}/get/")
-    ScratchAsset.create!(filename: asset_name, project_id: nil, uploaded_user_id: nil)
-                .file
-                .attach(io: StringIO.new(asset.body), filename: asset_name)
+    create_scratch_asset
+    save_to_editor_asset_bucket
   rescue StandardError => e
     Rails.logger.error("Failed to import asset #{asset_name}: #{e.message}")
+  end
+
+  def asset
+    @asset ||= begin
+      sleep(ASSET_FETCHING_DELAY)
+      connection.get("#{asset_name}/get/")
+    end
+  end
+
+  def create_scratch_asset
+    return if ScratchAsset.global_assets.exists?(filename: asset_name)
+
+    io = StringIO.new(asset.body)
+
+    ScratchAsset.create!(filename: asset_name, project_id: nil, uploaded_user_id: nil)
+                .file
+                .attach(io:, filename: asset_name)
+  end
+
+  def save_to_editor_asset_bucket
+    return unless save_to_editor_asset_bucket?
+
+    body = StringIO.new(asset.body)
+
+    s3_client.put_object(
+      bucket: ENV.fetch('EDITOR_ASSETS_BUCKET'),
+      key: asset_key,
+      body:,
+      content_type: asset_content_type
+    )
+  end
+
+  def save_to_editor_asset_bucket?
+    return false unless ENV['EDITOR_ASSETS_BUCKET']
+
+    s3_client.head_object(bucket: ENV.fetch('EDITOR_ASSETS_BUCKET'), key: asset_key)
+    false
+  rescue Aws::S3::Errors::NotFound
+    true
+  end
+
+  def asset_key
+    "internalapi/asset/#{asset_name}/get/"
+  end
+
+  def asset_content_type
+    extension = File.extname(asset_name).delete('.')
+    Mime::Type.lookup_by_extension(extension).to_s
+  end
+
+  def s3_client
+    @s3_client ||= Aws::S3::Client.new(
+      access_key_id: ENV.fetch('EDITOR_ASSETS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('EDITOR_ASSETS_SECRET_ACCESS_KEY'),
+      endpoint: ENV.fetch('EDITOR_ASSETS_ENDPOINT'),
+      region: 'auto'
+    )
   end
 
   def connection
