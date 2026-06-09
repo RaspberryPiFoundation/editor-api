42c2ef2d9da663da316e40ece538d80691be6dd7
Ram Modhvadia <ram.modhvadia@raspberrypi.org>
2026-06-09 11:43:21 +0100

    update sb3 parser to pull files from zip

--- lib/sb3_parser.rb
+++ lib/sb3_parser.rb
@@ -23,8 +23,8 @@ class Sb3Parser
       content = JSON.parse(project_json.get_input_stream.read)
 
       output = {
-        scratch_component: { content: }
-        # assets: assets(zip_file, extract_asset_names(content))
+        scratch_component: { content: },
+        assets: assets(zip_file, extract_asset_names(content))
       }
       output
     end
@@ -45,34 +45,32 @@ class Sb3Parser
     zip_file.find_entry('project.json') || raise(MissingProjectJsonError, 'project.json not found in SB3 archive')
   end
 
-  # def extract_asset_names(value)
-  #   case value
-  #   when Hash
-  #     names = []
-  #     names << value['md5ext'] if value['md5ext'].is_a?(String)
-  #     value.each_value { |item| names.concat(extract_asset_names(item)) }
-  #     names.uniq
-  #   when Array
-  #     value.flat_map { |item| extract_asset_names(item) }.uniq
-  #   else
-  #     []
-  #   end
-  # end
+  def extract_asset_names(value)
+    case value
+    when Hash
+      names = []
+      names << value['md5ext'] if value['md5ext'].is_a?(String)
+      value.each_value { |item| names.concat(extract_asset_names(item)) }
+      names.uniq
+    when Array
+      value.flat_map { |item| extract_asset_names(item) }.uniq
+    else
+      []
+    end
+  end
 
-  # def assets(zip_file, asset_names)
-  #   entries_by_name = zip_file.each.reject(&:directory?).index_by { |entry| entry.name }
+  def assets(zip_file, asset_names)
+    asset_names.map do |asset_name|
+      entry = zip_file.find_entry(asset_name) || raise(MissingAssetError, "asset #{asset_name} not found in SB3 archive")
+      asset(entry)
+    end
+  end
 
-  #   asset_names.map do |asset_name|
-  #     entry = entries_by_name[asset_name] || raise(MissingAssetError, "asset #{asset_name} not found in SB3 archive")
-  #     asset(entry)
-  #   end
-  # end
+  def asset(entry)
+    io = StringIO.new(entry.get_input_stream.read)
+    content_type = Marcel::MimeType.for(io, name: entry.name)
+    io.rewind
 
-  # def asset(entry)
-  #   io = StringIO.new(entry.get_input_stream.read)
-  #   content_type = Marcel::MimeType.for(io, name: entry.name)
-  #   io.rewind
-
-  #   { filename: entry.name, io:, content_type: }
-  # end
+    { filename: entry.name, io:, content_type: }
+  end
 end
