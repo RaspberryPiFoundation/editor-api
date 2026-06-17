0e86060902b4b14132dffa0120e7e5a6eea27131
Matthew Trew <matthew.trew@raspberrypi.org>
2026-06-17 15:28:12 +0200

    Revert 642b752

--- config/initializers/logging.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-# Ensure Semantic Logger is used in all cases by updating cached references to the standard logger.
-Rails.application.config.after_initialize do
-  ActiveSupport::LogSubscriber.logger = Rails.logger
-  Rails.application.env_config['action_dispatch.logger'] = Rails.logger
-end
