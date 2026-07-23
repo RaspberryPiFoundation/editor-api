3d3c0a0f50a7c594a2d4437e63e09fcf7f69bc72
Matthew Trew <matthew.trew@raspberrypi.org>
2026-07-23 16:50:27 +0100

    Remove local to-file JSON log appender
    This doesn't seem to work well at lower log levels.

--- config/application.rb
+++ config/application.rb
@@ -79,7 +79,6 @@ module App
         # Log to STDOUT on Heroku in JSON format, where this variable is set automatically.
         appenders.add(io: $stdout, formatter: :json, application: "editor-api@#{ENV['HEROKU_SLUG_COMMIT'] || 'unknown'}")
       else
-        appenders.add(file_name: "log/#{Rails.env}.log", formatter: :json)
         appenders.add_server(formatter: :color)  # $stdout while serving
         appenders.add_console(formatter: :color) # $stderr inside `rails console`
       end
