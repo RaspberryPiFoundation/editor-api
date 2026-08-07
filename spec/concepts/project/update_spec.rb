62e6553a85ea64d05cd1bcfab88d6e21680155b0
Chris Zetter <253059100+zetter-rpf@users.noreply.github.com>
2026-08-07 10:40:46 +0100

    Move student instructions restriction into strong params
    Previously Project::Update compared old and new instructions to
    detect and reject a student trying to change them, needing a
    current_user argument and a JSON-normalising comparison just to
    tell Parameters and plain Ruby values apart.
    
    This change drops :instructions from the permitted params entirely
    when the current user is a student, so their attempt is filtered
    out the same way Rails treats any other unpermitted param. The
    request now succeeds with the change silently ignored, instead of
    returning a 422. Project::Update no longer needs to know who the
    current user is.

--- spec/concepts/project/update_spec.rb
+++ spec/concepts/project/update_spec.rb
@@ -9,10 +9,9 @@ RSpec.describe Project::Update, type: :unit do
       components: component_hash,
       instructions:
     }
-    described_class.call(project:, update_hash:, current_user:)
+    described_class.call(project:, update_hash:)
   end
 
-  let(:current_user) { create(:user) }
   let!(:project) { create(:project, :with_default_component, :with_components) }
   let(:editable_component) { project.components.last }
   let(:default_component) { project.components.first }
@@ -97,10 +96,9 @@ RSpec.describe Project::Update, type: :unit do
       end
     end
 
-    context 'when the instructions have changed and the current user is a teacher' do
+    context 'when the instructions have changed' do
       let(:school) { create(:school) }
-      let!(:current_user) { create(:teacher, school:) }
-      let!(:project) { create(:project, :with_instructions, school:, user_id: current_user.id) }
+      let!(:project) { create(:project, :with_instructions, school:, user_id: create(:teacher, school:).id) }
       let(:instructions) { 'new instructions' }
 
       it 'returns success? true' do
@@ -109,29 +107,6 @@ RSpec.describe Project::Update, type: :unit do
 
       it 'updates project instructions' do
         expect { update }.to change { project.reload.instructions }.to('new instructions')
-      end
-    end
-
-    context 'when the instructions have changed and the current user is a student' do
-      let(:school) { create(:school) }
-      let!(:current_user) { create(:student, school:) }
-      let!(:project) { create(:project, :with_instructions, school:, user_id: current_user.id) }
-      let(:instructions) { 'new instructions' }
-
-      it 'returns success? false' do
-        expect(update.success?).to be(false)
-      end
-
-      it 'does not update project name' do
-        expect { update }.not_to change { project.reload.name }
-      end
-
-      it 'does not update project instructions' do
-        expect { update }.not_to change { project.reload.instructions }
-      end
-
-      it 'returns an error message' do
-        expect(update[:error]).to eq('Student cannot update project instructions')
       end
     end
   end
