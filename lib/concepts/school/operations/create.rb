4da8f280e42f95ad9c2b7b37b66bb8df0cfe05f4
Jamie Benstead <jamie.benstead@gmail.com>
2026-08-17 09:57:24 +0100

    Remove comment

--- lib/concepts/school/operations/create.rb
+++ lib/concepts/school/operations/create.rb
@@ -15,7 +15,7 @@ class School
 
         response
       rescue ProfileApiClient::UnauthorizedError => e
-        # Do not log noise to sentry. The Profile API is only available to verified users over 13.
+        # Do not log noise to sentry.
         # TODO: consider returning a separate error here to distinguish from other errors and return 401 from the API, not 422
         Rails.logger.warn { "Failed to onboard school #{response[:school].id}: user is unauthorized" }
         failure(response, e)
