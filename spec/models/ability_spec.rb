2086c9168dd0e97b75c94be1257a4c7155e008ae
Nathan Richards <nathan.richards@irysan.com>
2026-08-19 18:13:47 +0200

    test: cover student preview permission with persisted role

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -161,6 +161,34 @@ RSpec.describe Ability do
       end
     end
 
+    context 'with a persisted school student' do
+      let(:school) { create(:school) }
+      let(:user) { create(:user, id: user_id) }
+      let(:public_scratch_starter) do
+        build(:project,
+              user_id: nil,
+              school_id: nil,
+              project_type: Project::Types::CODE_EDITOR_SCRATCH)
+      end
+      let(:public_python_starter) do
+        build(:project,
+              user_id: nil,
+              school_id: nil,
+              project_type: Project::Types::PYTHON)
+      end
+      let(:public_scratch_component) { build(:component, project: public_scratch_starter) }
+      let(:public_python_component) { build(:component, project: public_python_starter) }
+
+      before do
+        create(:student_role, user_id: user.id, school:)
+      end
+
+      it { is_expected.to be_able_to(:show, public_scratch_starter) }
+      it { is_expected.not_to be_able_to(:show, public_python_starter) }
+      it { is_expected.to be_able_to(:show, public_scratch_component) }
+      it { is_expected.not_to be_able_to(:show, public_python_component) }
+    end
+
     context 'with an experience-cs admin' do
       let(:user) { build(:experience_cs_admin_user, id: user_id) }
       let(:another_project) { build(:project) }
