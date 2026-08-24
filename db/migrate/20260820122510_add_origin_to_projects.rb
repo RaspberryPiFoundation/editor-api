8943e3c67cade01e9db3119ed2b59c1f07c4122f
cocomarine <hj.kang@raspberrypi.org>
2026-08-24 14:56:43 +0100

    update migration to add index concurrently

--- db/migrate/20260820122510_add_origin_to_projects.rb
+++ db/migrate/20260820122510_add_origin_to_projects.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 class AddOriginToProjects < ActiveRecord::Migration[8.1]
+  disable_ddl_transaction!
+
   def change
-    add_column :projects, :origin, :string
-    add_index :projects, :origin, where: 'origin IS NOT NULL'
+    add_column :projects, :origin, :string, if_not_exists: true
+    add_index :projects, :origin, where: 'origin IS NOT NULL',
+                                  algorithm: :concurrently,
+                                  if_not_exists: true
   end
 end
